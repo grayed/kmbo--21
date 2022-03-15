1976e89f7f8861a80f75a9c3c49ba815ea68418c
Vadim Zhukov <persgray@gmail.com>
2022-03-15 15:17:30 +0300

    classwork address arithmetics WIP

--- classwork/main.cpp
+++ classwork/main.cpp
@@ -1,10 +1,25 @@
 ﻿#include <iostream>
-
-#include "main.h"
-
 using namespace std;
+
+class A {
+    string a_s;
+    friend int main();
+public:
+    string getBString() const { /* TODO: return B::b_s value */; }
+};
+class B : public A {
+    string b_s;
+    friend int main();
+public:
+    B() { b_s = "It's b!"; }
+};
 
 int main()
 {
+    B b;
+    A *a = &b;
+    cout << "b address is " << &b << ", a_s address is " << &b.a_s << ", b_s address is " << &b.b_s << endl;
+    cout << "b size is " << sizeof(b) << ", a_s size is " << sizeof(b.a_s) << endl;
+    cout << "b_s is '" << a->getBString() << "'" << endl;
 	return 0;
 }
