32091cbd4c4b64ca3c23fb0c3b983b12001761a0
Алексей Поляков <alexxxlolkekmem@mail.ru>
2022-03-22 13:49:42 +0300

    Здраствуйте, Поляков АА КМБО-03-21. Практическая 2

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -1,36 +1,56 @@
+#pragma once
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-
 class vector3d {
+public:
 	float data[3];
 public:
 	vector3d() { data[2] = data[1] = data[0] = 0; }
 	vector3d(float value) { data[2] = data[1] = data[0] = value; }
 	vector3d(float a1, float a2, float a3) { data[0] = a1;  data[1] = a2; data[2] = a3; }
 
-	float  operator[](int idx) const { return data[idx]; }
-	float& operator[](int idx)       { return data[idx]; }
-	// TODO: operator + (vector3d, vector3d), operator - (vector3d, vector3d), operator * (vector3d, float)
-	vector3d operator +(const vector3d& other) { return vector3d(/* ... */); }
-};
+	float  operator [](int index) const { return data[index]; }
+	float& operator [](int index) { return data[index]; }
 
-vector3d operator -(const vector3d& v1, const vector3d& v2) {
-	return vector3d(/* ... */);
-}
+	vector3d operator +(const vector3d& other) { return vector3d(data[0] + other[0], data[1] + other[1], data[2] + other[2]); }
+	vector3d operator -(const vector3d& other) { return vector3d(data[0] - other[0], data[1] - other[1], data[2] - other[2]); }
+	vector3d operator *(const float value) { return vector3d(data[0] * value, data[1] * value, data[2] * value); }
+	vector3d operator /(const float value) { return vector3d(data[0] / value, data[1] / value, data[2] / value); }
+	vector3d operator -() { return vector3d(-data[0], -data[1], -data[2]); }
+	vector3d operator !()
+	{
+		if ((data[0] == 0) && (data[1] == 0) && (data[2] == 0))
+		{
+			return vector3d(data[0] = 1, data[1] = 1, data[2] = 1);
+		}
+		else return vector3d(data[0] = 0, data[1] = 0, data[2] = 0);
+	}
+};
 
 ostream& operator <<(ostream& os, const vector3d& v) {
 	return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }";
 }
-
-int main(int argc, char** argv) {
-	vector3d v1, v2(12), v3(1, 3, 8);
-	v1[2] = 54;
-	/* TODO implement +, - and * operators first
-	vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v1 * 0.5f;
-	cout << "v4: " << v4 << endl;
-	cout << "v5: " << v5 << endl;
-	cout << "v6: " << v6 << endl;
-	*/
-	return 0;
+bool test_vector3d()
+{
+	vector3d v1(5, 5, 5);
+	vector3d v2(1, 1, 1);
+	vector3d v3;
+	vector3d v4(0, 0, 0);
+	v3 = v1 + v2;
+	if ((v3.data[0] != 6) || (v3.data[1] != 6) || (v3.data[2] != 6)) { cout << "Операция сложения не работает. Данные проверки v1(5,5,5), v2(1,1,1)"; return false; }
+	v3 = v1 - v2;
+	if ((v3.data[0] != 4) || (v3.data[1] != 4) || (v3.data[2] != 4)) { cout << "Операция вычитания не работает. Данные проверки v1(5,5,5), v2(1,1,1)"; return false; }
+	v3 = v1 * 3;
+	if ((v3.data[0] != 15) || (v3.data[1] != 15) || (v3.data[2] != 15)) { cout << "Операция умножения на скаляр не работает. Данные проверки v1(5,5,5), 3"; return false; }
+	v3 = v1 / 5;
+	if ((v3.data[0] != 1) || (v3.data[1] != 1) || (v3.data[2] != 1)) { cout << "Операция деления на скаляр не работает. Данные проверки v1(5,5,5), 5"; return false; }
+	v3 = -v1;
+	if ((v3.data[0] != -5) || (v3.data[1] != -5) || (v3.data[2] != -5)) { cout << "Операция инверсии не работает. Данные проверки v1(5,5,5)"; return false; }
+	v3 = !v1;
+	if ((v3.data[0] != 0) || (v3.data[1] != 0) || (v3.data[2] != 0)) { cout << "Операция ! не работает. Данные проверки v4(0,0,0)"; return false; }
+	v3 = !v4;
+	if ((v3.data[0] != 1) || (v3.data[1] != 1) || (v3.data[2] != 1)) { cout << "Операция ! не работает. Данные проверки v1(1,1,1)"; return false; }
+	return true;
 }
+
--- vectors/vector.h
+++ vectors/vector.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	if (test_vector3d()) cout << " " << 0;
+	else cout << " " << 1;
+}
