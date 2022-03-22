a4daacc94356a3a1402ceac85c98d0378d8f59d4
Никита Кулаков <akulakov11@gmail.com>
2022-03-22 16:45:56 +0300

    Добавил инвертирование,унарные операции и деление. И функцию  тест для операторов класса Vector3d

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -1,17 +1,17 @@
 #include "vector.h"
 #include <iostream>
-#include <cmath>
-
-using namespace std;
-
-   
+using namespace std; 
 int main() {
-	vector3d v1, v2(12), v3(1, 3, 8);
+	Vector3d v1, v2(12), v3(1, 3, 8);
 	v1[2] = 54;
-	vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v2 * 0.5f;
+	Vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v2 * 0.5f, v7 = -v7,v8=!v8,v9= v2/2.f;
 	cout << "v4: " << v4 << endl;
 	cout << "v5: " << v5 << endl;
 	cout << "v6: " << v6 << endl;
+	cout << "v7: " << v7 << endl;
+	cout << "v8: " << v8 << endl;
+	cout << "v9: " << v9 << endl;
+	
 	return 0;
 }
 
--- vectors/vector.h
+++ vectors/vector.h
@@ -1,36 +1,60 @@
 #pragma once
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
-class vector3d {
+
+class Vector3d {
 public:
 	float data[3];
 public:  
-	vector3d() { data[2] = data[1] = data[0] = 0; }
-	vector3d(float value) { data[2] = data[1] = data[0] = value; }
-	vector3d(float a1, float a2, float a3) { data[0] = a1;  data[1] = a2; data[2] = a3; }
-
+	Vector3d() { data[2] = data[1] = data[0] = 0; }
+	Vector3d(float value) { data[2] = data[1] = data[0] = value; }
+	Vector3d(float a1, float a2, float a3) { data[0] = a1;  data[1] = a2; data[2] = a3; }
 	float  operator [](int index) const { return data[index]; }
 	float& operator [](int index) { return data[index]; }
-    /* Либо так:
-	vector3d operator +(const vector3d& other) { return vector3d(data[0] + other[0], data[1] + other[1], data[2] + other[2]); }
-	vector3d operator -(const vector3d& other) { return vector3d(data[0] - other[0], data[1] - other[1], data[2] - other[2]); }
-	vector3d operator *(const float value) { return vector3d(data[0] * value, data[1] * value, data[2] * value); } 
-	*/
-};
-// Либо так:
-vector3d operator+ (const vector3d& v1, const vector3d& v2) {
-	return vector3d((v1.data[0] + v2.data[0]), (v1.data[1] + v2.data[1]), (v1.data[2] + v2.data[2]));
-}
-vector3d operator- (const vector3d& v1, const vector3d& v2) {
-	return vector3d((v1.data[0] - v2.data[0]), (v1.data[1] - v2.data[1]), (v1.data[2] - v2.data[2]));
-}
-float operator* (const vector3d& v, float value) { vector3d(v.data[1] * value, v.data[0] * value, v.data[2] * value); }
+	//
+	Vector3d operator +(const Vector3d& other) { return Vector3d(data[0] + other[0], data[1] + other[1], data[2] + other[2]); }
+	//
+	Vector3d operator -(const Vector3d& other) { return Vector3d(data[0] - other[0], data[1] - other[1], data[2] - other[2]); }
+	//
+	Vector3d operator *(const float value) { return Vector3d(data[0] * value, data[1] * value, data[2] * value); }
+	//
+	Vector3d operator /(const float value) { 
+		if (value != 0) return Vector3d(data[0] / value,data[1]/value,data[2]/value);
+		else cout << "Введите значение не равное нулю";
+	}
+	//
+	Vector3d operator -() { Vector3d(-data[0], -data[1], -data[2]); }
+	//
+	Vector3d operator !() {
+		if (data[0] == 0 && data[1] == 0 && data[2] == 0) return Vector3d(1, 1, 1);
+		else return Vector3d(0, 0, 0);
+	}
+	
 
-ostream& operator <<(ostream& os, const vector3d& v) {
+};
+
+
+ostream& operator <<(ostream& os, const Vector3d& v) {
 	return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }";
 }
+
+
+bool test_all_operators() {
+	Vector3d v1;
+	Vector3d v2(5, 5, 5);
+	Vector3d v3(1, 1, 1);
+	Vector3d  v4 = v1 + v2, v5 = v3 - v2, v6 = v2 * 3, v7 = v2 / 5, v8 = -v2, v9 = !v3, v10 = !v1;
+	if (v4.data[0] != 5 || v4.data[1] != 5 || v4.data[2] != 5)		{ cout << "Addition Error";		return false; }
+	if (v5.data[0] != 4 || v5.data[1] != 4 || v5.data[2] != 4)		{ cout << "Substraction Error"; return false; }
+	if (v6.data[0] != 15 || v6.data[1] != 15 || v6.data[2] != 15)	{ cout << "Multiplying Error";	return false; }
+	if (v7.data[0] != 1 || v7.data[1] != 1 || v7.data[2] != 1)		{ cout << "Divising Error";		return false; }
+	if (v8.data[0] != -5 || v8.data[1] != -5 || v8.data[2] != -5)	{ cout << "Inverting Error";	return false; }
+	if (v9.data[0] != 0 || v3.data[1] != 0 || v9.data[2] != 0)		{ cout << "Boolean Converting Error";	return false; }
+	if (v10.data[0] != 1 || v10.data[1] != 1 || v10.data[2] != 1)	{ cout << "Boolean Converting Error";	return false; }
+	return true;
+}
+
 
 
