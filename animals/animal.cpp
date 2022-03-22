6da9363740ec5d9a2b4384f1530adc20654e069f
Алексей Поляков <alexxxlolkekmem@mail.ru>
2022-03-22 11:09:27 +0300

    1 Практическая Поляков А. А. КМБО-03-21

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -1,7 +1,15 @@
 #include "animal.h"
-
+#include <sstream>
+#include <iostream>
 using namespace std;
 
 int main() {
-	return 0;
+    Cat Scottish(5);
+    Horse Spirit(1234);
+    Scottish.set_MiceCaughtCounter(7);
+    Spirit.setkilometresRun(1500);
+    int res = Scottish.get_MiceCaughtCounter();
+    int res2 = Spirit.get_kilometresRun();
+    cout << res << " " << res2 << "\n";
+    return 0;
 }
--- animals/animal.h
+++ animals/animal.h
@@ -1,18 +1,79 @@
 #pragma once
-
 #include <iostream>
+#include <sstream>
+using namespace std;
+
 
 class Animal {
+private:
+    bool CanMoveFreely;
 public:
-	float weight;				// kg
+    Animal() { CanMoveFreely = 0; }
+    Animal(bool YesOrNo) { CanMoveFreely = YesOrNo; }
+    void setCanMoveFreely(bool YesOrNo) { CanMoveFreely = YesOrNo; }
+    bool get_CanMoveFreely() const { return CanMoveFreely; }
+    virtual string about() const { return (stringstream() << "CanMoveFreely =" << CanMoveFreely).str(); }
+
 };
 
-class Mammal : public Animal {
+
+class Mammal :public Animal {
+private:
+    bool FeedsWithMilk;
 public:
-	float pregnancyDuration;	// days
+    Mammal() { FeedsWithMilk = 0; }
+    Mammal(bool FeedOrNot) { FeedsWithMilk = FeedOrNot; }
+    void setFeedsWithMilk(bool FeedOrNot) { FeedsWithMilk = FeedOrNot; }
+    bool get_FeedsWithMilk() const { return FeedsWithMilk; }
+    virtual string about() const { return (stringstream() << Animal::about() << ", " << "FeedsWithMilk =" << FeedsWithMilk).str(); }
 };
 
-class Cat : public Mammal {
+class Cat :public Mammal {
+private:
+    int MiceCaughtCounter;
 public:
-	float vibrissaLength;		// meters
+    Cat(int MiceCaught) { MiceCaughtCounter = MiceCaught; }
+    void set_MiceCaughtCounter(int MiceCaught) { MiceCaughtCounter = MiceCaught; }
+    int get_MiceCaughtCounter() const { return MiceCaughtCounter; }
+    virtual string about() const { return (stringstream() << Animal::about() << ", " << Mammal::about() << ", " << "MiceCaughtCounter =" << MiceCaughtCounter).str(); }
+};
+
+class Horse :public Mammal {
+private:
+    int kilometresRun;
+public:
+    Horse(int distance) { kilometresRun = distance; }
+    void setkilometresRun(int distance) { kilometresRun = distance; }
+    int get_kilometresRun() const { return kilometresRun; }
+    virtual string about() const { return (stringstream() << Animal::about() << ", " << Mammal::about() << ", " << "kilometresRun =" << kilometresRun).str(); }
+};
+
+
+class Birds : public Animal {
+private:
+    int FeathersCounter;
+public:
+    Birds() { FeathersCounter = 0; }
+    Birds(int Feathers) { FeathersCounter = Feathers; }
+    void setFeathersCounter(int Feathers) { FeathersCounter = Feathers; }
+    int get_FeathersCounter() const { return FeathersCounter; }
+    virtual string about() const { return (stringstream() << Animal::about() << ", " << "FeathersCounter =" << FeathersCounter).str(); }
+};
+class Pigeon :public Birds {
+private:
+    bool CanBegForBread;
+public:
+    Pigeon(int CheekyOrNot) { CanBegForBread = CheekyOrNot; }
+    bool get_CanBegForBread() const { return CanBegForBread; }
+    void setCanBegForBread(int CheekyOrNot) { CanBegForBread = CheekyOrNot; }
+    virtual string about() const { return (stringstream() << Animal::about() << ", " << Birds::about() << ", " << "CanBegForBread =" << CanBegForBread).str(); }
+};
+class Caliber :public Birds {
+private:
+    int WingBeatSpeed;
+public:
+    Caliber(int BeatCounter) { WingBeatSpeed = BeatCounter; }
+    int get_WingBeatSpeed() const { return WingBeatSpeed; }
+    void setWingBeatSpeed(int BeatCounter) { WingBeatSpeed = BeatCounter; }
+    virtual string about() const { return (stringstream() << Animal::about() << ", " << Birds::about() << ", " << "WingBeatSpeed =" << WingBeatSpeed).str(); }
 };
