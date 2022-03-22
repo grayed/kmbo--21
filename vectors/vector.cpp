ecbf92d23918cf2602f70bad17f30671650c3f3d
Никита Кулаков <akulakov11@gmail.com>
2022-03-22 16:48:16 +0300

    Cpp file for Vector

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -5,12 +5,13 @@ int main() {
 	Vector3d v1, v2(12), v3(1, 3, 8);
 	v1[2] = 54;
 	Vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v2 * 0.5f, v7 = -v7,v8=!v8,v9= v2/2.f;
-	cout << "v4: " << v4 << endl;
-	cout << "v5: " << v5 << endl;
-	cout << "v6: " << v6 << endl;
-	cout << "v7: " << v7 << endl;
-	cout << "v8: " << v8 << endl;
-	cout << "v9: " << v9 << endl;
+	cout << "v4: " << v4 << "\n";
+	cout << "v5: " << v5 << "\n";
+	cout << "v6: " << v6 << "\n";
+	cout << "v7: " << v7 << "\n";
+	cout << "v8: " << v8 << "\n";
+	cout << "v9: " << v9 << "\n";
+	cout << test_all_operators()<<"\n";
 	
 	return 0;
 }
