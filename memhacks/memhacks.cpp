11530b65a7ae7a525de5fc1f0761e55515d3915c
Vadim Zhukov <persgray@gmail.com>
2022-03-23 17:22:52 +0300

    add memhacks homework

---
+++ memhacks/memhacks.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include "memhacks.h"
+
+using namespace std;
+
+// 1. Реализовать в A функцию-геттер, возвращающую значение B::b_s.
+//    Используйте знание о расположении объектов в памяти.
+//
+// 2. Реализовать A::printData(), выводящую в поток текствое представление A::a_s, B::b_s и B::data
+//    с помощью A::getBString() и аналогичных техник.
+//
+// 3. Реализовать A::printData2(), выводящую в поток текствое представление A::a_s, B::b_s и B::data
+//    с помощью виртуальных функций.
+
+B::B() : b_s("It's b!") {
+	for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+		data[i] = i * 2;
+}
+
+/// <summary>
+/// Выводит на экран адреса и размеры объекта типа <see cref="B"/> и его содержимого.
+/// Можно модифицировать для собственных отладочных целей.
+/// </summary>
+/// <param name="b">Изучаемый объект</param>
+void printInternals(const B& b) {
+	const A* a = &b, * a2 = a + 1;
+	cout << "Address of b is 0x" << &b << ", address of b.a_s is 0x" << &b.a_s << ", address of b.b_s is 0x" << &b.b_s << endl;
+	cout << "Size of A is " << sizeof(A) << ", size of B is " << sizeof(B) << endl;
+	cout << "B string is '" << b.getBString() << "'" << endl;
+	//cout << "B data: "; b.printData(cout); cout << endl;
+}
+
+/// <summary>
+/// Извлекает значение <see cref="B::b_s"/> из текущего объекта.
+/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
+/// </summary>
+/// <returns>Значение B::b_s</returns>
+std::string A::getBString() const {
+	// TODO
+}
+
+/// <summary>
+/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
+/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
+/// с помощью адресной арифметики.
+/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
+/// </summary>
+void A::printData(std::ostream& os) {
+	// TODO
+}
+
+/// <summary>
+/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
+/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
+/// с помощью виртуальных функций, предусмотренных в классе <see cref="A"/>.
+/// </summary>
+void A::printData2(std::ostream& os) {
+	// TODO
+}
+
+int main()
+{
+	B b;
+	printInternals(b);
+	return 0;
+}
