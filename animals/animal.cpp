09b9ebfdef4f69aa4e45054adb09d29f506a43af
Elter30 <tsvv2003@gmail.com>
2022-03-30 09:43:08 +0300

    Задание животные 1,2

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
+	Pinguin Lolo(35.75, 70, 35.5);
+
+	Ostrich Pepe(80.85, 2000, 65);
+
+	Crocodile Gena(101, true, "Brown");
+
+	Chameleon Vasya(0.2, false, 3.5);
+
 	return 0;
 }
