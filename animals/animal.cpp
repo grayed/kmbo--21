11af7ab9f4cc18d3b469d04a0500b082159d5b27
hitfot <90093220+hitfot@users.noreply.github.com>
2022-04-05 14:53:56 +0300

    Update animal.cpp

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main() {
-	return 0;
+	Cat cat;
+	Dog dog;
+	Raven raven;
+	Sparrow sparrow;
+	cat.setpregnancyDuration(70);
+	cat.setvibrissaLength(0.2);
+	cat.setweight(2);
+	dog.setpregnancyDuration(80);
+	dog.setrazorLength(0.3);
+	dog.setweight(30);
+	raven.setfearthLength(0.7);
+	raven.setheight(0.3);
+	raven.setweight(1);
+	sparrow.setfearthLength(0.1);
+	sparrow.setrazorLength(0.02);
+	sparrow.setweight(0.5);
 }
