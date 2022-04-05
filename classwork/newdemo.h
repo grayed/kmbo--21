379cb4b08d740f79b56b3d4802854e41b617d681
Vadim Zhukov <persgray@gmail.com>
2022-04-05 17:32:42 +0300

    improve local code

--- classwork/newdemo.h
+++ classwork/newdemo.h
@@ -1,11 +1,32 @@
-#ifndef NEWDEMO_H
-#define NEWDEMO_H
+#pragma once
 
+#include <sys/types.h>
 
-class newdemo
+class Cls
 {
+    static int next_id;
+    int id;
+
 public:
-    newdemo();
+    Cls();
+    ~Cls();
+
+    static void* operator new[](size_t size);
+    static void  operator delete[](void *p);
 };
 
-#endif // NEWDEMO_H
+/**
+ * 1. Реализовать operator new и
+ *    operator delete в классе Cls.
+ *
+ * 2. Реализовать в new/delete кеширование
+ *    памяти из-под одного уничтоженного
+ *    объекта: delete заполняет кеш (если
+ *    он не заполнен), а new по возможности
+ *    этот кеш использует. Подсказка:
+ *
+ *    private:
+ *      static void *cache;
+ *
+ * 3. Повторить п.2 для new[]/delete[].
+ */
