7efc06f1af9a1a346c4e1e521ee34e24647e6798
Egor Osipov <exilegrief@gmail.com>
2022-04-05 14:40:39 +0300

    .пп

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main() {
+	Amphibia amphibia;
+	amphibia.setNumber_of_limbs(4);
+	cout << amphibia.getNumber_of_limbs() << "\n";
+
+	Wolf wolf;
+	wolf.setAverage_life_expectancy(17);
+	cout << wolf.getAverage_life_expectancy() << "\n";
+
+	Frog frog;
+	frog.setNumber_of_offspring(4);
+	cout << frog.getNumber_of_offspring() << "\n";
+
+	Hare hare;
+	hare.setRunning_speed_km_ch(12);
+	cout << hare.getRunning_speed_km_ch() << "\n";
 	return 0;
+
+	cout << amphibia.about();
 }
