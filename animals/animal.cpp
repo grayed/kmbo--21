1e916deacee09abbc307b0b6569382dccfa6f261
kendoow <kendo_ow@mail.ru>
2022-04-17 22:46:49 +0300

    1 and 2 practice

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -1,7 +1,16 @@
 #include "animal.h"
-
+#include <string>
 using namespace std;
 
 int main() {
-	return 0;
+	Human Ivan(false,89);
+	Ivan.setIq(101);
+	Ivan.setIsWorking(true);
+	string desc = Ivan.about();
+	cout << "Human : " << desc << endl;
+	Parrot Kesha(true);
+	Kesha.setLivesInCity(false);
+	Kesha.setCanFly(true);
+	string descParrot = Kesha.about();
+	cout << "Parrot :" << descParrot << endl;
 }
--- animals/animal.h
+++ animals/animal.h
@@ -1,18 +1,177 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
-class Animal {
+class Animal
+{
+private:
+	int age;	// в годах :)
+	int weight; // в кг
+	int height; // в метрах
+
 public:
-	float weight;				// kg
+	Animal(){};
+	Animal(int Age, int Weight, int Height)
+	{
+		Age = age;
+		Weight = weight;
+		Height = height;
+	}
+
+	void setAge(float Age) { age = Age; }
+	void setWeight(float Weight) { weight = Weight; }
+	void setHeight(int Height) { height = Height; }
+
+	float getAge() { return age; }
+	float getWeight() { return weight; }
+	int getHeight() { return height; }
+	virtual string about() const
+	{
+		stringstream str;
+		str << "Age = " << age << "; "
+			<< "Weight = " << weight << "; "
+			<< "Height = " << height << endl;
+		return str.str();
+	}
 };
 
-class Mammal : public Animal {
+// млекопитающее
+class Mammal : public Animal
+{
+private:
+	string woolColor; // цвет шерсти
+	bool predator;	  // хищник да/нет
 public:
-	float pregnancyDuration;	// days
-};
+	Mammal(){};
+	Mammal(string WoolColor, bool Predator)
+	{
+		woolColor = WoolColor;
+		predator = Predator;
+	}
+	string getWoolColor() { return woolColor; }
+	void setWoolColor(int value) { woolColor = value; }
+	bool getPredator() { return predator; }
+	void setPredator(bool value) { predator = value; }
 
-class Cat : public Mammal {
+	virtual string about() const
+	{
+		stringstream ss;
+		ss << "woolColor = " << woolColor << ", "
+		   << "Predaor = " << predator;
+		return ss.str();
+	}
+};
+// человек
+class Human : public Mammal
+{
+private:
+	bool isWorking; // устроен ли на работу да/ нет
+	int iq;			// Коэффициент интеллекта до 150
+
 public:
-	float vibrissaLength;		// meters
+	Human(bool IsWorking, int Iq)
+	{
+		isWorking = IsWorking;
+		iq = Iq;
+	}
+
+	bool getIsWorking() { return isWorking; };
+	void setIsWorking(bool value) { isWorking = value; }
+	int getIq() { return iq; };
+	void setIq(int value) { iq = value; }
+	virtual string about()
+	{
+		stringstream ss;
+		ss << "iq = " << getIq() << ", "
+		   << "IsWorking = " << getIsWorking();
+		return ss.str();
+	}
+};
+class Deer : public Mammal
+{
+private:
+	int lengthOfHorns; // длина рогов в см
+
+public:
+	Deer(){};
+	Deer(int LengthOfHorns)
+	{
+		lengthOfHorns = LengthOfHorns;
+	}
+	int getLengthOfHorns() { return lengthOfHorns; };
+	void setLengthOfHorns(int value) { lengthOfHorns = value; }
+	virtual string about()
+	{
+		stringstream ss;
+		ss << "lengthOfHorns = " << getLengthOfHorns();
+		return ss.str();
+	}
+};
+class Bird : public Animal
+{
+private:
+	bool canFly; // может летать да/ нет
+	bool canSwim; // может плавать да/нет
+
+public:
+	Bird(){};
+	Bird(bool CanFly, bool CanSwim)
+	{
+		canFly = CanFly;
+		canSwim = CanSwim;
+	}
+	bool getCanFly() { return canFly; }
+	void setCanFly(bool value) { canFly = value; }
+	bool getCanSwim() { return canSwim; }
+	void setCanSwim(int value) { canSwim = value; }
+	virtual string about() const
+	{
+		stringstream ss;
+		ss << "Can fly = " << getCanFly << ", "
+		   << "Can swim = " << getCanSwim;
+		return ss.str();
+	}
+};
+class Parrot : public Bird
+{
+private: 
+	bool livesInCity; // живет в городе да/нет
+
+public:
+	Parrot(){}
+	Parrot(bool LivesInCity)
+	{
+		livesInCity = LivesInCity;
+	}
+	int getLivesInCity() { return livesInCity; };
+	void setLivesInCity(int value) { livesInCity = value; }
+	virtual string about()
+	{
+		stringstream ss;
+		ss << "Can fly = " << getCanFly() << ", "
+		   << "Lives in city = " << getLivesInCity();
+		return ss.str();
+	}
+};
+class Eagle : public Bird
+{
+private:
+	int speed; // в км/ч
+public:
+	Eagle() {};
+	Eagle(int Speed)
+	{
+		speed = Speed;
+	}
+	int getSpeed() { return speed; };
+	void setSpeed(int value) { speed = value; }
+	virtual string about()
+	{
+		stringstream ss;
+		ss << "can fly = " << getCanFly() << ", "
+		   << "speed = " << getSpeed();
+		return ss.str();
+	}
 };
