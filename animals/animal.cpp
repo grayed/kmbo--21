bc26f851994c4034a5116ea7595e8bd1f55b92e9
chernikovtimofey <chernikovtimofeyg@gmail.com>
2022-04-22 11:30:30 +0300

    animals solution

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -2,6 +2,85 @@
 
 using namespace std;
 
+Animal::Animal(float weight_) {
+	weight = weight_;
+}
+
+string Animal::about() const{
+	stringstream ss;
+	ss << "weight = " << weight;
+	return ss.str();
+}
+
+ostream& operator <<(ostream& os, const Animal& animal) {
+	return os << animal.about();
+}
+
+Mammal::Mammal(float weight_, float pregnancyDuration_) : Animal(weight_) {
+	pregnancyDuration = pregnancyDuration_;
+}
+
+string Mammal::about() const {
+	stringstream ss;
+	ss << Animal::about() << " pregnancyDuration = " << pregnancyDuration;
+	return ss.str();
+}
+
+Insect::Insect(float weight_, float antennasLength_) : Animal(weight_) {
+	antennasLength = antennasLength_;
+}
+
+string Insect::about() const {
+	stringstream ss;
+	ss << Animal::about() << " antennasLength = " << antennasLength;
+	return ss.str();
+}
+
+Cat::Cat(float weight_, float pregnancyDuration_, float vibrissaLength_) : Mammal(weight_, pregnancyDuration_) {
+	vibrissaLength = vibrissaLength_;
+}
+
+string Cat::about() const {
+	stringstream ss;
+	ss << Mammal::about() << " vibrissaLength = " << vibrissaLength;
+	return ss.str();
+}
+
+Dog::Dog(float weight_, float pregnancyDuration_, float tailLength_) : Mammal(weight_, pregnancyDuration_) {
+	tailLength = tailLength_;
+}
+
+string Dog::about() const {
+	stringstream ss;
+	ss << Mammal::about() << " tailLength = " << tailLength;
+	return ss.str();
+}
+
+Bug::Bug(float weight_, float antennasLength_, float wingLength_) : Insect(weight_, antennasLength_) {
+	wingLength = wingLength_;
+}
+
+string Bug::about() const {
+	stringstream ss;
+	ss << Insect::about() << " wingLength = " << wingLength;
+	return ss.str();
+}
+
+Dragonfly::Dragonfly(float weight_, float antennasLength_, float airspeed_) : Insect(weight_, antennasLength_) {
+	airspeed = airspeed_;
+}
+
+string Dragonfly::about() const {
+	stringstream ss;
+	ss << Insect::about() << " airspeed = " << airspeed;
+	return ss.str();
+}
+
 int main() {
-	return 0;
+	Cat cat = Cat(3.9, 60, 0.007);
+	Dog dog = Dog(40, 68, 0.018);
+	Bug bug = Bug(0.154, 0.003, 0.022);
+	Dragonfly dragonfly = Dragonfly(0.011, 0.0008, 27);
+
+	cout << cat << endl << dog << endl << bug << endl << dragonfly << endl;
 }
--- animals/animal.h
+++ animals/animal.h
@@ -1,18 +1,102 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+
+using namespace std;
 
 class Animal {
-public:
+private:
 	float weight;				// kg
+
+protected:
+	Animal(float weight_);
+
+public:
+	float getWeight() const { return weight; }
+	void setWeigh(float newValue) { weight = newValue; }
+
+	virtual string about() const;
 };
 
+ostream& operator <<(ostream& os, const Animal& animal);
+
 class Mammal : public Animal {
-public:
+private:
 	float pregnancyDuration;	// days
+
+protected:
+	Mammal(float weight_, float pregnanceDuration_);
+
+public:
+	float getPregnancyDuration() const { return pregnancyDuration; }
+	void setPregnancyDuration(float newValue) { pregnancyDuration = newValue; }
+
+	virtual string about() const;
+};
+
+class Insect : public Animal {
+private:
+	float antennasLength; 		// meters
+
+protected:
+	Insect(float weight_, float antennasLength_);
+
+public:
+	float getAntennasLenght() const { return antennasLength; }
+	void setAntennasLength(float newValue) { antennasLength = newValue; }
+
+	virtual string about() const;
 };
 
 class Cat : public Mammal {
-public:
+private:
 	float vibrissaLength;		// meters
+
+public:
+	Cat(float weight_, float pregnancyDuration_, float vibrissaLength_);
+
+	float getVibrissaLength() const { return vibrissaLength; }
+	void setVibrissaLength(float newValue) {vibrissaLength = newValue; }
+
+	virtual string about() const;
+};
+
+class Dog : public Mammal {
+private:
+	float tailLength; 			// meters
+
+public:
+	Dog(float weight_, float pregnancyDuration_, float tailLength);
+
+	float getTailLength() const { return tailLength; }
+	void setTailLength(float newValue) { tailLength = newValue; }
+
+	virtual string about() const;
+};
+
+class Bug : public Insect {
+private:
+	float wingLength; 			// meters
+
+public:
+	Bug(float weight_, float antennasLength_, float wingLength_);
+
+	float getWingLength() const { return wingLength; }
+	void setWingLength(float newValue) { wingLength = newValue; }
+
+	virtual string about() const;
+};
+
+class Dragonfly : public Insect {
+private:
+	float airspeed; 			// meters per second
+
+public:
+	Dragonfly(float weight_, float antennasLength_, float airspeed_);
+
+	float getAirSpeed() const { return airspeed; }
+	void setAirSpeed(float newValue) { airspeed = newValue; }
+
+	virtual string about() const;
 };
