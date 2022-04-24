197d042e63226ce905b08004f841e2ef0efcf8e7
Anastasia <90093556+somniiium@users.noreply.github.com>
2022-04-24 16:26:05 +0300

    Update animal.cpp

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -1,7 +1,22 @@
 #include "animal.h"
-
+#include <string>
 using namespace std;
 
-int main() {
-	return 0;
-}
+int main() 
+{
+	Dog Lana("brown");
+	Lana.setСoat_color("black");
+	string aboutDog = Lana.about();
+	cout << "Lana : " << aboutDog << endl;
+
+	Snake Eve(true);
+	Eve.setMolt(false);
+	string MoltEve = Eve.about();
+	cout << "Snake : " << MoltEve << endl;
+
+	Turtle Sage(26, 40);
+	Sage.setAge(137);
+	Sage.setNumber_life(752);
+	string truth_about_Sage = Sage.about();
+	cout << "Sage : " << truth_about_Sage << endl;
+}  
