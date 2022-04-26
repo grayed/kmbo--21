4e883fe5b7975f498b53d4ad49742a856766672d
xephosbot <xephosbot@gmail.com>
2022-04-26 13:49:55 +0300

    123

--- vectors/array.cpp
+++ vectors/array.cpp
@@ -1,31 +1,10 @@
 #include "array.h"
-<<<<<<< HEAD
 
 template<size_t dim>
 vector<dim> average(const vector<dim> a, const vector<dim> b) {
     vector<dim> temp;
     for (int i = 0; i < dim; ++i) temp[i] = (a[i] + b[i]) / 2;
     return temp;
-=======
-
-using namespace std;
-
-// Написать шаблонные функции:
-//   1. operator + (реализация внутри класса)
-//   2. operator * (вектор на скаляр и скаляр на вектор; реализация вне класса)
-//   3. Нахождение среднего для двух векторов (реализация вне класса)
-
-ostream& operator <<(ostream& os, const Vector3d& v) {
-    return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }";
-}
-
-template<size_t Dimensions>
-ostream& operator <<(ostream& os, const Vector<Dimensions>& v) {
-    os << "{ " << v[0];
-    for (size_t i = 1; i < Dimensions; i++)
-        os << ", " << v[i];
-    return os << " }";
->>>>>>> 1d6eac8230aca4ab7bde98408de994c59fe11542
 }
 
 int main() {
