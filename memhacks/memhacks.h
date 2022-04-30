92076bf8461d3faac06e8c9e8b4a2a241ca949ea
xephosbot <xephosbot@gmail.com>
2022-04-30 17:26:31 +0300

    Игры с памятью №1

--- memhacks/memhacks.h
+++ memhacks/memhacks.h
@@ -9,22 +9,29 @@ class A {
 	std::string a_s;
 	int foo;
 
-	friend void printInternals(const B&);
+	friend void printInternals(B &b);
 
 public:
-	std::string getBString() const;
+    A();
+
+	virtual std::string getBString() const;
+    virtual float getData(int idx) const;
+
 	void printData(std::ostream& os);
 	void printData2(std::ostream& os);
 };
 
 class B : public A {
 	std::string b_s;
-	float data[7];
+	float data[7]{};
 
-	friend void printInternals(const B&);
+	friend void printInternals(B &b);
 
 public:
 	B();
+
+    float getData(int idx) const override;
+    std::string getBString() const override;
 };
 
-void printInternals(const B& b);
+void printInternals(B &b);
