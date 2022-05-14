51a0bb13e41cfe1987e53171b3080b6d73afc9b0
Владислав Букин <vlad244244@gmail.com>
2022-05-14 18:14:55 +0300

    Была изменена функция test_vector

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -36,84 +36,107 @@ const vector3d operator !(const vector3d& v1) {
     else
         return vector3d(0, 0, 0);
 }
-bool test_vector3d(vector <vector3d> Vec, vector3d v1, vector3d v2, float v3) {
-    bool check = true;
-    for (int i = 0; i < Vec.size(); ++i) {
-        switch (i)
-        {
-        case 0:
-            if ((Vec[i][0] == (v1[0] + v2[0])) && (Vec[i][1] == (v1[1] + v2[1])) && (Vec[i][2] == (v1[2] + v2[2])))
-                cerr << "Test 1 binary vector addition " << v1 << " and " << v2 << " - Correct " << endl;
-            else
-            {
-                cerr << "Test binary vector addition " << v1 << " and " << v2 << " - Incorrect" << endl;
-                check = false;
-            }
-            break;
-
-        case 1:
-            if ((Vec[i][0] == (v1[0] - v2[0])) && (Vec[i][1] == (v1[1] - v2[1])) && (Vec[i][2] == (v1[2] - v2[2])))
-                cerr << "Test 2 binary difference of vectors " << v1 << " and " << v2 << " - Correct" << endl;
-            else
-            {
-                cerr << "Test 2 binary difference of vectors " << v1 << " and " << v2 << " - Incorrect" << endl;
-                check = false;
-            }
-            break;
-        case 2:
-            if ((Vec[i][0] == (v1[0] * v3)) && (Vec[i][1] == (v1[1] * v3)) && (Vec[i][2] == (v1[2] * v3)))
-                cerr << "Test 3 binary operation of multiplying a vector by a scolar " << v1 << " на " << v3 << " - Correct" << endl;
-            else
-            {
-                check = false;
-                cerr << "Test 3 binary operation of multiplying a vector by a scolar " << v1 << " на " << v3 << " - Incorrect" << endl;
-            }
-            break;
-        case 3:
-            if ((Vec[i][0] == (v1[0] / v3)) && (Vec[i][1] == (v1[1] / v3)) && (Vec[i][2] == (v1[2] / v3)))
-                cerr << "Test 4 binary operation of dividing a vector by a scolar " << v1 << " на " << v3 << " - Correct" << endl;
-            else
-            {
-                check = false;
-                cerr << "Test 4 binary operation of dividing a vector by a scolar " << v1 << " на " << v3 << " - Incorrect" << endl;
-            }
-            break;
-        case 4:
-            if ((Vec[i][0] == (-v1[0])) && (Vec[i][1] == (-v1[1])) && (Vec[i][2] == (-v1[2])))
-                cerr << "Test 5 unary operation of inverting the vector sign  " << v1 << " - Correct" << endl;
-            else
-            {
-                check = false;
-                cerr << "Test 5 unary operation of inverting the vector sign  " << v1 << " - Incorrect" << endl;
-            }
-            break;
-
-        default:
-            vector3d v(0, 0, 0);
-            if (i == 5)
-                v = v1;
-            if (v[0] == 0 && v[1] == 0 && v[2] == 0) {
-                if ((Vec[i][0] == 1) && (Vec[i][1] == 1) && (Vec[i][2] == 1)) {
-                    cerr << "Test" << i + 1 << ": unary ! vector " << v << " - Correct" << endl;
-                }
-                else {
-                    check = false;
-                    cerr << "Test" << i + 1 << ": unary ! vector " << v << " - Incorrect" << endl;
-                }
-            }
-            else {
-                if ((Vec[i][0] == 0) && (Vec[i][1] == 0) && (Vec[i][2] == 0)) {
-                    cerr << "Test" << i + 1 << ": unary ! vector " << v << " - Correct" << endl;
-                }
-                else {
-                    check = false;
-                    cerr << "Test" << i + 1 << ": unary ! vector " << v << " - Incorrect" << endl;
-                }
-            }
-            break;
+bool test_vector_addition(vector3d& v1, vector3d& v2) {
+    bool flag = true;
+    vector3d v3 = v1 + v2;
+    for (int i = 0; i < 3; ++i) {
+        if((v3[0] == (v1[0]+v2[0])) && (v3[1] == (v1[1]+v2[1])) && (v3[2] == (v1[2]+v2[2])))
+            cerr << "Test 1 binary vector addition " << v1 << " and " << v2 << " - Correct " << endl;
+        else {
+            cerr << "Test binary vector addition " << v1 << " and " << v2 << " - Incorrect" << endl;
+            flag = false;
         }
+        break;
     }
-    return check;
+    return flag;
+}
+
+bool test_vector_difference(vector3d& v1, vector3d& v2) {
+    bool flag = true;
+    vector3d v3 = v1 - v2;
+    for (int i = 0; i < 3; ++i) {
+        if ((v3[0] == (v1[0] - v2[0])) && (v3[1] == (v1[1] - v2[1])) && (v3[2] == (v1[2] - v2[2])))
+            cerr << "Test 2 binary difference of vectors " << v1 << " and " << v2 << " - Correct" << endl;
+        else {
+            cerr << "Test 2 binary difference of vectors " << v1 << " and " << v2 << " - Incorrect" << endl;
+            flag = false;
+        }
+        break;
+    }
+    return flag;
+}
+
+bool test_vector_multiplying(vector3d& v1, float& c1) {
+    bool flag = true;
+    vector3d v3 = v1 * c1;
+    for (int i = 0; i < 3; ++i) {
+        if ((v3[0] == (v1[0] * c1 )) && (v3[1] == (v1[1] * c1)) && (v3[2] == (v1[2] *c1)))
+            cerr << "Test 3 binary operation of multiplying a vector by a scolar " << v1 << " на " << c1 << " - Correct" << endl;
+        else {
+            cerr << "Test 3 binary operation of multiplying a vector by a scolar " << v1 << " на " << c1 << " - Incorrect" << endl;
+            flag = false;
+        }
+        break;
+    }
+    return flag;
+}
+
+
+bool test_vector_dividing(vector3d& v1, float& c1) {
+    bool flag = true;
+    vector3d v3 = v1 / c1;
+    for (int i = 0; i < 3; ++i) {
+        if ((v3[0] == (v1[0] / c1)) && (v3[1] == (v1[1] / c1)) && (v3[2] == (v1[2] / c1)))
+            cerr << "Test 4 binary operation of dividing a vector by a scolar " << v1 << " на " << c1 << " - Correct" << endl;
+        else {
+            cerr << "Test 4 binary operation of dividing a vector by a scolar " << v1 << " на " << c1 << " - Incorrect" << endl;
+            flag = false;
+        }
+        break;
+    }
+    return flag;
+}
+
+bool test_vector_inverting(vector3d& v1) {
+    bool flag = true;
+    vector3d v3 = - v1;
+    for (int i = 0; i < 3; ++i) {
+        if ((v3[0] == -v1[0]) && (v3[1] == -v1[1]) && (v3[2] == -v1[2]))
+            cerr << "Test 5 unary operation of inverting the vector sign  " << v1 << " - Correct" << endl;
+        else {
+            cerr << "Test 5 unary operation of inverting the vector sign  " << v1 << " - Incorrect" << endl;
+            flag = false;
+        }
+        break;
+    }
+    return flag;
+}
+bool test_vector_denial() {
+    bool flag = true;
+    vector3d v1 = (0, 0, 0);
+    vector3d v3 = !v1;
+    for (int i = 0; i < 3; ++i) {
+        if ((v3[0] == 1) && (v3[1] == 1) && (v3[2] == 1))
+            cerr << "Test" << i + 1 << ": unary ! vector " << v1 << " - Correct" << endl;
+        else {
+            cerr << "Test" << i + 1 << ": unary ! vector " << v1 << " - Incorrect" << endl;
+            flag = false;
+        }
+        break;
+    }
+    return flag;
+}
+
+
+bool test_vector3d(vector3d v1, vector3d v2, float c1) {
+    bool flag = true;
+    test_vector_addition(v1, v2);
+    test_vector_difference(v1, v2);
+    test_vector_multiplying(v1, c1);
+    test_vector_dividing(v1, c1);
+    test_vector_inverting(v1);
+    test_vector_denial();
+    return flag;
 }
 
 int main(int x, char** y) {
@@ -128,6 +151,6 @@ int main(int x, char** y) {
     v9 = !v1; Vect.push_back(v9);
     v10 = !v9; Vect.push_back(v10);
     cout << " First : " << v4 << " Second : " << v5 << " Third : " << v6 << " Fourth : " << v7 << " Fifth : " << v8 << " Sixth : " << v9 << " Seventh : " << v10 << endl;
-    bool Check = test_vector3d(Vect, v1, v2, v3);
+    bool Check = test_vector3d(v1, v2, v3);
     return 0;
 }
