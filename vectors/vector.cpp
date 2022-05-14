f96ee0be72a8aea152609d19a221ae392952d29f
Peter357648 <90093729+Peter357648@users.noreply.github.com>
2022-05-14 18:47:10 +0300

    Векторы

--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -1,44 +1,152 @@
 #include <iostream>
-#include "vector.h"
-
+//#include "vector.h"
 using namespace std;
 
 class vector3d {
-	float data[3];
+    float data[3];
 public:
-	vector3d() { data[2] = data[1] = data[0] = 0; }
-	vector3d(float value) { data[2] = data[1] = data[0] = value; }
-	vector3d(float a1, float a2, float a3) { data[0] = a1;  data[1] = a2; data[2] = a3; }
-	// lvalue = rvalue
-	float  operator[] (int idx) const { return data[idx]; }
-	float& operator[] (int idx)       { return data[idx]; } // подразумевает изменение
-	vector3d operator +(const vector3d& other) const { 
-		return vector3d(data[0] + other.data[0], data[1] + other.data[1], other.data[2] + data[2]);
-	}
-	//friend ostream& operator <<(ostream& os, const vector3d& v);
+    vector3d() { data[2] = data[1] = data[0] = 0; }
+    vector3d(float value) { data[2] = data[1] = data[0] = value; }
+    vector3d(float a1, float a2, float a3) { data[0] = a1;  data[1] = a2; data[2] = a3; }
+    float  operator[](int idx) const { return data[idx]; }
+    float& operator[](int idx)       { return data[idx]; }
 };
-
-vector3d operator -(const vector3d& v1, const vector3d& v2) {
-	return vector3d(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+vector3d operator + (const vector3d& v1, const vector3d& v2) { return vector3d(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]); }
+vector3d operator - (const vector3d& v1, const vector3d& v2) { return vector3d(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]); }
+vector3d operator * (const vector3d& v1, const float x)      { return vector3d(v1[0] * x, v1[1] * x, v1[2] * x); }
+vector3d operator * (const float x, const vector3d& v1)      { return vector3d(v1[0] * x, v1[1] * x, v1[2] * x); }
+vector3d operator / (const vector3d& v1, const float x)      { return vector3d(v1[0] / x, v1[1] / x, v1[2] / x); }
+const vector3d operator -(const vector3d& v1)                { return vector3d(-v1[0], -v1[1], -v1[2]); }
+const vector3d operator !(const vector3d& v1) {
+    if (v1[0] == 0 && v1[1] == 0 && v1[2] == 0)              { return vector3d(1, 1, 1); }
+    else                                                     { return vector3d(0, 0, 0); }
 }
 
-ostream& operator <<(ostream& os, const vector3d& v) {
-	return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }";
+
+ostream& operator <<(ostream& os, const vector3d& v)         { return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }"; }
+
+
+void testBVA(vector3d firstTester, vector3d secondTester, bool flag) {
+    vector3d ans;
+    ans[0] = (firstTester[0] + secondTester[0]);
+    ans[1] = (firstTester[1] + secondTester[1]);
+    ans[2] = (firstTester[2] + secondTester[2]);
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = firstTester + secondTester;
+    cerr << test << " - Vector obtained from + operator." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Binary Vector Addition went successful." << endl << endl;
+    } else { cerr << "ERROR in Binary Vector Addition: something went wrong." << endl << endl; flag = 0; }
 }
+
+void testBVS(vector3d firstTester, vector3d secondTester, bool flag) {
+    vector3d ans;
+    ans[0] = (firstTester[0] - secondTester[0]);
+    ans[1] = (firstTester[1] - secondTester[1]);
+    ans[2] = (firstTester[2] - secondTester[2]);
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = firstTester - secondTester;
+    cerr << test << " - Vector obtained from - operator." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Binary Vector Subtraction went successful." << endl << endl;
+    } else { cerr << "ERROR in Binary Vector Subtraction: something went wrong." << endl << endl; flag = 0; }
+}
+
+void testLeftVSM(float floatTester, vector3d secondTester, bool flag) {
+    vector3d ans;
+    ans[0] = (floatTester * secondTester[0]);
+    ans[1] = (floatTester * secondTester[1]);
+    ans[2] = (floatTester * secondTester[2]);
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = floatTester * secondTester;
+    cerr << test << " - Vector obtained from * operator (left)." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Vector Scalar Multiple (left) went successful." << endl << endl;
+    } else { cerr << "ERROR in Vector Scalar Multiple (left): something went wrong." << endl << endl; flag = 0; }
+}
+
+void testRightVSM(vector3d firstTester, float floatTester, bool flag) {
+    vector3d ans;
+    ans[0] = (firstTester[0] * floatTester);
+    ans[1] = (firstTester[1] * floatTester);
+    ans[2] = (firstTester[2] * floatTester);
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = firstTester * floatTester;
+    cerr << test << " - Vector obtained from * operator (right)." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Vector Scalar Multiple (right) went successful." << endl << endl;
+    } else { cerr << "ERROR in Vector Scalar Multiple (right): something went wrong." << endl << endl; flag = 0; }
+}
+
+void testVSD(vector3d firstTester, float floatTester, bool flag) {
+    vector3d ans;
+    ans[0] = (firstTester[0] / floatTester);
+    ans[1] = (firstTester[1] / floatTester);
+    ans[2] = (firstTester[2] / floatTester);
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = firstTester / floatTester;
+    cerr << test << " - Vector obtained from / operator." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Vector Scalar Division went successful." << endl << endl;
+    } else { cerr << "ERROR in Vector Scalar Division: something went wrong." << endl << endl; flag = 0; }
+}
+
+void testCV(vector3d firstTester, bool flag) {
+    vector3d ans;
+    ans[0] = (-firstTester[0]);
+    ans[1] = (-firstTester[1]);
+    ans[2] = (-firstTester[2]);
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = -firstTester;
+    cerr << test << " - Vector obtained from - operator." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Converse Vector went successful." << endl << endl;
+    } else { cerr << "ERROR in Converse Vector: something went wrong." << endl << endl; flag = 0; }
+}
+
+void testLDV(vector3d firstTester, bool flag) {
+    vector3d ans;
+    if (!(firstTester[0] || firstTester[0] || firstTester[0])) { ans[0] = ans[1] = ans[2] = 1; }
+
+    cerr << ans << " - Vector with answers." << endl;
+
+    vector3d test = !firstTester;
+    cerr << test << " - Vector obtained from ! operator." << endl;
+
+    if ((ans[0] == test[0]) && (ans[1] == test[1]) && (ans[2] == test[2])) {
+        cerr << "Testing of Logical Denial of Vector went successful." << endl << endl;
+    } else { cerr << "ERROR in Logical Denial of Vector: something went wrong." << endl << endl; flag = 0; }
+}
+
+
+bool test_vector3d(vector3d v1, vector3d v2) {
+    bool flag = 1;
+    testBVA(v1, v2, flag);
+    testBVS(v1, v2, flag);
+    testLeftVSM(3, v2, flag);
+    testRightVSM(v1, 3, flag);
+    testVSD(v1, -1.7, flag);
+    testCV(v2, flag);
+    testLDV(v2, flag);
+    if (flag) { cout << "Tests passed successfully!" << endl; }
+    else { cout << "GREAT ERROR!!!" << endl; }
+    return true;
+}
+
 
 int main(int argc, char** argv) {
-	vector3d v1, v2(12), v3(1, 3, 8);
-	v1[2] = 54;
-	//vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v1 * 0.5f;
-	//cout << "v4: " << v4 << endl << "v5: " << v5 << endl << "v6: " << v6 << endl;
-
-	printf("address of v1:             0x%p, size: %zu bytes\n", &v1, sizeof(v1));
-	printf("address of v1.data:        0x%p, size: %zu bytes\n", &v1.data, sizeof(v1.data));
-	printf("address of v1.data[-1]:    0x%p, size: %zu bytes\n", &v1.data[-1], sizeof(v1.data[-1]));
-	printf("address of v1.data[0]:     0x%p, size: %zu bytes\n", &v1.data[0], sizeof(v1.data[0]));
-	printf("address of v1.data[1]:     0x%p, size: %zu bytes\n", &v1.data[1], sizeof(v1.data[1]));
-	printf("address of v1.data[2]:     0x%p, size: %zu bytes\n", &v1.data[2], sizeof(v1.data[2]));
-	printf("address of v1.data[2000]:  0x%p, size: %zu bytes\n", &v1.data[2000], sizeof(v1.data[2000]));
-
-	return 0;
+    vector3d v1(2, 2, 2), v2(4, 4, 4);
+    test_vector3d(v1, v2);
 }
