500a12c06e508671254308c6f5ab5d1ef3fb20e3
eshendo <eshendo@yandex-team.ru>
2022-05-17 10:44:02 +0300

    memhacks 1

--- memhacks/memhacks.h
+++ memhacks/memhacks.h
@@ -9,22 +9,28 @@ class A {
 	std::string a_s;
 	int foo;
 
-	friend void printInternals(const B&);
+	friend void printInternals( B&);
 
 public:
 	std::string getBString() const;
 	void printData(std::ostream& os);
 	void printData2(std::ostream& os);
+
+	virtual std::string getBs() const {};
+	virtual float * getBData() {};
 };
 
 class B : public A {
 	std::string b_s;
 	float data[7];
 
-	friend void printInternals(const B&);
+	friend void printInternals( B&);
 
 public:
 	B();
+
+	std::string getBs() const { return b_s; }
+	float * getBData() { return data; }
 };
 
-void printInternals(const B& b);
+void printInternals( B& b);
