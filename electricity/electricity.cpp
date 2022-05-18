5cd4ff3cfe51e1a984844e320f482c4977b4612d
eshendo <eshendo@yandex-team.ru>
2022-05-18 08:21:51 +0300

    fix diff 2

--- electricity/electricity.cpp
+++ electricity/electricity.cpp
@@ -5,41 +5,20 @@ using namespace std;
 
 bool Object::isConnectedTo(const Object& other) const
 {
-    for (size_t j = 0; j < getPoleCount(); j++)
-    {
-        const Pole * selfPole = getPole(j);
-        if (selfPole != nullptr && (selfPole->connectedObject) == &other) return true;
-    }   
-
+    // TODO
     return false;
 }
 
 bool Object::connect(const std::string& poleName, Object& other, const std::string& otherPoleName)
 {
-    if (poleName == otherPoleName && &other == this) return false;
-
-    Pole * selfPole = getPole(poleName);
-    Pole * otherPole = other.getPole(otherPoleName);
-    if (otherPole == nullptr || otherPole == nullptr) return false;
-
-    (selfPole->connectedObject) = &other;
-    (selfPole->connectedObjectPole) = otherPoleName;
-    
-    (otherPole->connectedObject) = this;
-    (otherPole->connectedObjectPole) = poleName;
-
-    return true;
+    // TODO
+    return false;
 }
 
 bool Object::disconnect(const std::string& poleName)
 {
-    Pole * p = getPole(poleName);
-    if (p == nullptr) return false;
-
-    (p->connectedObject) = nullptr;
-    (p->connectedObjectPole) = "";
-    
-    return true;
+    // TODO
+    return false;
 }
 
 Switch::Switch(const std::string& name)
@@ -60,8 +39,7 @@ const Pole* Switch::getPole(const string& name) const
 
 const Pole* Switch::getPole(size_t idx) const
 {
-    if (idx == 0) return &a1;
-    else if (idx == 1) return &a2;
+    // TODO
     return nullptr;
 }
 
@@ -72,15 +50,6 @@ int main()
     cout << "is " << (sw.isConnectedTo(sw2) ? "" : "not ") << "connected" << endl;
 
     // TODO: создать цепь из генератора, выключателя и светильника
-    Generator g; Light l; Switch sw3;
-
-    g.connect("p", l, "A1");
-    l.connect("A2", sw3, "A1");
-    cout << "is " << (g.isConnectedTo(l) ? "" : "not ") << "connected" << endl;
-    cout << "is " << (l.isConnectedTo(sw3) ? "" : "not ") << "connected" << endl;
-
-    g.disconnect("p");
-    cout << "is " << (g.isConnectedTo(l) ? "" : "not ") << "connected" << endl;
 
     return 0;
 }
--- vectors/vector.cpp
+++ vectors/vector.cpp
@@ -1,42 +1,25 @@
 #include <iostream>
 #include "vector.h"
 
-class vector3d {
-    float data[3];
+using namespace std;
 
-    public:
-        vector3d() { data[0] = data[1] = data[2] = 0; }
-        vector3d(float value) { data[0] = data[1] = data[2] = value; }
-        vector3d(float x, float y, float z) { data[0] = x; data[1] = y; data[2] = z; }
-
-        float operator[](int ind) const {
-
-            if (ind > 2 || ind < 0) throw std::overflow_error("index out of bounds");
-
-            return data[ind]; 
-        }
-
-        float& operator[](int ind) {
-
-            if (ind > 2 || ind < 0) throw std::overflow_error("index out of bounds");
-
-            return data[ind]; 
-        }
-
-        vector3d operator +(const vector3d& other) { return vector3d(data[0] + other[0], data[1] + other[1], data[2] + other[2]); }
-        vector3d operator -(const vector3d& other) { return vector3d(data[0] - other[0], data[1] - other[1], data[2] - other[2]); }
-        vector3d operator *(const float value) { return vector3d(data[0] * value, data[1] * value, data[2] * value); }
-};
-
-std::ostream& operator <<(std::ostream& os, const vector3d& v) {
-    return os << "{" << v[0] << ", " << v[1] << ", " << v[2] << "}";
+ostream& operator <<(ostream& os, const vector3d& v) {
+	return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }";
 }
 
-int main() {
-    vector3d v1, v2(12), v3(1, 3, 8);
-    v1[2] = 54;
-    vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v1 * 0.5f;
-    std::cout << "v4: " << v4 << std::endl;
-    std::cout << "v5: " << v5 << std::endl;
-    std::cout << "v6: " << v6 << std::endl;
+int main(int argc, char** argv) {
+	vector3d v1, v2(12), v3(1, 3, 8);
+	v1[2] = 54;
+	//vector3d v4 = v1 + v2, v5 = v1 - v2, v6 = v1 * 0.5f;
+	//cout << "v4: " << v4 << endl << "v5: " << v5 << endl << "v6: " << v6 << endl;
+
+	printf("address of v1:             0x%p, size: %zu bytes\n", &v1, sizeof(v1));
+	printf("address of v1.data:        0x%p, size: %zu bytes\n", &v1.data, sizeof(v1.data));
+	printf("address of v1.data[-1]:    0x%p, size: %zu bytes\n", &v1.data[-1], sizeof(v1.data[-1]));
+	printf("address of v1.data[0]:     0x%p, size: %zu bytes\n", &v1.data[0], sizeof(v1.data[0]));
+	printf("address of v1.data[1]:     0x%p, size: %zu bytes\n", &v1.data[1], sizeof(v1.data[1]));
+	printf("address of v1.data[2]:     0x%p, size: %zu bytes\n", &v1.data[2], sizeof(v1.data[2]));
+	printf("address of v1.data[2000]:  0x%p, size: %zu bytes\n", &v1.data[2000], sizeof(v1.data[2000]));
+
+	return 0;
 }
