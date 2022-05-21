72169372bd1797eaa5db2ed84fa3ae1a56716f32
Никита Кулаков <akulakov11@gmail.com>
2022-05-21 14:42:18 +0300

    Решение задачи newhacks

--- memhacks/memhacks.h
+++ memhacks/memhacks.h
@@ -2,37 +2,111 @@
 
 #include <ostream>
 #include <string>
+#include <sstream>
+#include <iostream>
 
-/*
-1) Реализовать функцию A::getBString(), возвращающую значение B::b_s(подразумевается, 
-что объект, для которого вызывается, является объектом типа B).
-Используйте знание о расположении объектов в памяти.
-2) Реализовать A::printData(), выводящую в поток текствое представление A::a_s, B::b_s и B::data с помощью A::getBString() и аналогичных техник.
-3) Реализовать A::printData2(), выводящую в поток текствое представление A::a_s, B::b_s и B::data с помощью виртуальных функций.
-*/
 
-class B;	// чтобы можно было объявить printInternals() как friend в классе A
 
 class A {
+protected:
 	std::string a_s;
 	int foo;
-
-	friend void printInternals(const B&);
-
 public:
-	std::string getBString() const;
-	void printData(std::ostream& os);
-	void printData2(std::ostream& os);
+	A() : a_s("It's a!") { }
+	
+	std::string getAString() const { return *((const std::string*)(this+1)+2); }
+
+	/// <summary>
+	/// Извлекает значение <see cref="B::b_s"/> из текущего объекта.
+	/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
+	/// </summary>
+	/// <returns>Значение B::b_s</returns>
+	std::string getBString() const { return *((const std::string*)(this+1)); } //string 4 bytes
+
+	float getdataFloat(int i) const { return *((const float*)(this+1)-i); }
+
+
+
+	virtual std::string str_A() const {
+		std::stringstream ss;
+		ss << "String A: " << a_s;
+		return ss.str();
+	}
 };
+
+
 
 class B : public A {
+protected:
 	std::string b_s;
-	float data[7];
-
-	friend void printInternals(const B&);
-
+	int data[7];
 public:
-	B();
+	B() : b_s("It's b!") {
+		for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+			data[i] = i * 2;
+	}
+
+	virtual std::string str_B() {
+		std::stringstream ss;
+		ss << "String B: " << b_s << std::endl;
+		ss << "Data : ";
+		for (int i = 0;i < 7;i++) {
+			ss << data[i] << "; ";
+		}
+		std::cout << std::endl;
+		return ss.str();
+	}
+
+	
+
+	void printData2(std::ostream& os);
+	
+	
+
+	friend void printInternals(const B&); // Дружественный метод класса
 };
 
-void printInternals(const B& b);
+/// <summary>
+/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
+/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
+/// с помощью виртуальных функций, предусмотренных в классе <see cref="A"/>.
+/// </summary>
+void B::printData2(std::ostream& os) {
+	os << str_A();   // virtual function
+	os << str_B();
+}
+
+
+
+/// <summary>
+/// Выводит на экран адреса и размеры объекта типа <see cref="B"/> и его содержимого.
+/// Можно модифицировать для собственных отладочных целей.
+/// </summary>
+/// <param name="b">Изучаемый объект</param>
+void printInternals(const B& b) { //
+	const A* a = &b, * a2 = a + 1;
+	std::cout << "Address of b is 0x" << &b << ", address of b.a_s is 0x" << &b.a_s << ", address of b.b_s is 0x" << &b.b_s << std::endl;
+	std::cout << "Size of A is " << sizeof(A) << ", size of B is " << sizeof(B) << std::endl;
+	std::cout << "B string is '" << b.getBString() << "'" << std::endl;
+	//std::cout << "B data: "; b.printData(std::cout); std::cout << std::endl;
+}
+
+
+/// <summary>
+	/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
+	/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
+	/// с помощью адресной арифметики.
+	/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
+	/// </summary>
+void printData(std::ostream& os, const A& a, const B& b) {
+	os << "A string is '" << a.getAString() << "'" << std::endl;
+	os << "B string is '" << b.getBString() << "'" << std::endl;
+	os << "Data: ";
+	for (int i = 0;i < 7;i++) {
+		os << b.getdataFloat(i) << "; "; // почему именно 7?? это ведь метод класса А
+	}
+}
+
+
+
+
---
+++ memhacks/newhacks.cpp
@@ -0,0 +1,32 @@
+﻿#include "newhacks.h"
+
+using namespace std;
+
+int main() {
+    //объявление объектов в динамической памяти
+    //демонстрация работы переопределенных методов new,delete
+    Foo* obj1 = new Foo;
+    delete obj1;
+
+    obj1 = new Foo;
+    delete obj1;
+    Bar* obj2 = new Bar;
+    delete obj2;
+    obj2 = new Bar;
+    delete obj2;
+
+    //Buz* obj3 = new Buz;
+    //мы не можем обратиться к методу клааса Buz, тк мы объявили его в зоне protected
+    
+    //объявление объектов на стэке
+    Foo obj12, obj13;
+    obj12.show_adress();
+    obj13.show_adress();
+    Bar obj22, obj23;
+    obj22.show_adress(); // Вот так функция работает тк я ее объявил в теле класса
+    obj23.show_adress();
+    Buz obj32, obj33;
+    obj32.show_adress();
+    obj33.show_adress();
+    return 0;
+}
