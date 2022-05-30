a686a725e64d1339a584f9c1fdb0e78a22c77472
Nikgor2002 <ngniknik@mail.ru>
2022-05-30 14:00:36 +0300

    Электричество

--- electricity/electricity.cpp
+++ electricity/electricity.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 bool Object::isConnectedTo(const Object& other) const
 {
-    for (int i = 1; i <= getPoleCount(); i++) {
-        for (int j = 1; j <= other.getPoleCount(); j++) {
-            if (getPole(i)->connectedObjectPole == other.getPole(j)->name && getPole(i)->name != other.getPole(j)->name)
-                return true;
+    // TODO
+    for (size_t idx_1 = 0; idx_1 < this->getPoleCount(); idx_1++) {
+
+        for (size_t idx_2 = 0; idx_2 < other.getPoleCount(); idx_2++) {
+
+            if (this->getPole(idx_1)->name == other.getPole(idx_2)->name) return true;
+
         }
     }
     return false;
@@ -16,19 +19,22 @@ bool Object::isConnectedTo(const Object& other) const
 
 bool Object::connect(const std::string& poleName, Object& other, const std::string& otherPoleName)
 {
-    if (poleName != otherPoleName) {
-        for (int i = 1; i <= getPoleCount(); i++)
-        {
-            other.getPole(i)->connectedObjectPole = poleName; // strcpy()?
-            //strcpy(other.getPole(i)->connectedObjectPole, poleName);
-        }
-        return true;
-    }
+    // TODO
+    this->getPole(poleName)->connectedObjectPole = otherPoleName;
+    this->getPole(poleName)->connectedObject = &other;
+
+    other.getPole(otherPoleName)->connectedObject = this;
+    other.getPole(otherPoleName)->connectedObjectPole = poleName;
+
     return false;
 }
 
-    Pole(const string &name_) : name(name_), connectedObject(nullptr) {}
-};
+bool Object::disconnect(const std::string& poleName)
+{
+    // TODO
+    if (this->getPole(poleName)->connectedObjectPole != "") { this->getPole(poleName)->connectedObjectPole = ""; }
+    return false;
+}
 
 class Object {
     string name;
@@ -47,44 +53,32 @@ public:
     void connect(const string &poleName, const Object &other, const string &otherPoleName) { /* TODO */ }
 };
 
-const Pole* Switch::getPole(size_t idx) const //
+const Pole* Switch::getPole(size_t idx) const
 {
-    for (size_t i = 0; i < getPoleCount();i++)
-        if (i==idx) {
-            return getPole(idx);
-        }
+    // TODO
+
     return nullptr;
 }
 
 int main()
 {
-    Switch sw, sw2;
-    sw.connect("A2", sw2, "A1");
-    cout << "is " << (sw.isConnectedTo(sw2) ? "" : "not ") << "connected" << endl;
-    
-    Light svj1, svj2;
-    svj1.connect("A2", svj2, "A1");
-    cout << "is " << (svj1.isConnectedTo(svj2) ? "" : "not ") << "connected" << endl;
-    
+    Switch sw;
+    Power pwr;
+    Light lgt;
+    int counter;
+    lgt.getPole("lgt_in");
+    lgt.getPole("lgt_out");
+    pwr.getPole("phase");
+    pwr.getPole("ground");
+    pwr.getPole("neutral");
+    lgt.connect("lgt_in", pwr, "phase");
+    lgt.connect("lgt_out", pwr, "neutral");
+    /*for (int i = 0; i<100; i++)
+    {
+        cin >> counter;
+        cout << lgt.getPole("lgt_in")->name << lgt.getPole("lgt_out")->name << endl;
+        cout << lgt.getPole("lgt_in")->connectedObjectPole << lgt.getPole("lgt_out")->connectedObjectPole << endl;
+    };*/
     // TODO: создать цепь из генератора, выключателя и светильника
-
     return 0;
-}
-
-const Pole* Light::getPole(const std::string& name) const
-{
-    if (name == a1.name)
-        return &a1;
-    if (name == a2.name)
-        return &a2;
-    return nullptr;
-}
-
-const Pole* Light::getPole(size_t idx) const
-{
-    for (size_t i = 0; i < getPoleCount(); i++)
-        if (i == idx) {
-            return getPole(idx);
-        }
-    return nullptr;
 }
