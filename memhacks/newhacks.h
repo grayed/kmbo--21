29275dfafb87e966699508f246d4931449026b63
bubasooo <t118nt@gmail.com>
2022-05-31 08:10:31 +0300

    memhacks2

--- memhacks/newhacks.h
+++ memhacks/newhacks.h
@@ -1,2 +1,41 @@
 ﻿#pragma once
+#include <string>
+#include <sys/types.h>
 
+
+class Foo
+{
+    std::string apples;
+    int n;
+
+public:
+    Foo();
+    ~Foo();
+
+    static void* operator new[](size_t size);
+    static void operator delete[](void *p);
+};
+
+class Bar : public Foo{
+  std::string oranges;
+  float kg;
+
+public:
+  Bar();
+  ~Bar();
+
+  static void* operator new[](size_t size);
+  static void operator delete[](void *p);
+};
+
+class Buz : public Foo{
+  std::string bananas;
+  bool ripe;
+  static void* operator new[](size_t size);
+  static void* operator new(size_t size);
+public:
+  Buz();
+  ~Buz();
+
+  static void operator delete[](void *p);
+};
