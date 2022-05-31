14b02062dd2405b389642960f00a4f8087befbee
RomanPozdn <90093720+RomanPozdn@users.noreply.github.com>
2022-05-31 16:37:09 +0300

    Update animal.h

--- animals/animal.h
+++ animals/animal.h
@@ -1,18 +1,80 @@
 #pragma once
-
 #include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
 
 class Animal {
 public:
-	float weight;				// kg
+	bool alive;
+	string about() const {
+		stringstream ss;
+		ss << "alive - " << alive << endl;
+		return ss.str();
+	};
 };
+
 
 class Mammal : public Animal {
 public:
-	float pregnancyDuration;	// days
+	int milk_volume;
+	string about() const {
+		stringstream ss;
+		ss << Animal::about() << "milk_volume - " << milk_volume << endl;
+		return ss.str();
+	};
 };
 
-class Cat : public Mammal {
+class Spineless : public Animal {
 public:
-	float vibrissaLength;		// meters
+	bool no_spine;
+	string about() const {
+		stringstream ss;
+		ss << Animal::about() << "no_spine - " << no_spine << endl;
+		return ss.str();
+	};
 };
+
+class Monkey : public Mammal {
+public:
+	int tail_lenght;
+	string about() const {
+		stringstream ss;
+		ss << Mammal::about()  << "tail_lenght - " << tail_lenght << endl;
+		return ss.str();
+	};
+};
+
+class Cow : public Mammal {
+public:
+	bool white;
+	string about() const {
+		stringstream ss;
+		ss << Mammal::about() << "white - " << white << endl;
+		return ss.str();
+	};
+};
+
+class Worm : public Spineless {
+public:
+	int length;
+	string about() const {
+		stringstream ss;
+		ss << Spineless::about() << "length - " << length << endl;
+		return ss.str();
+	};
+};
+
+class Jellyfish : public Spineless {
+public:
+	bool transparent;
+	string about() const {
+		stringstream ss;
+		ss << Spineless::about() << "transparent - " << transparent << endl;
+		return ss.str();
+	};
+};
+
+inline ostream& operator <<(ostream& os, const Animal& animal) {
+	return os << animal.about();
+}
