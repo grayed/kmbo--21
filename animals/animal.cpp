7fed9612330e953c18228142cbe685bd81a86a03
RomanPozdn <90093720+RomanPozdn@users.noreply.github.com>
2022-05-31 16:36:38 +0300

    Update animal.cpp

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -1,8 +1,31 @@
-#include "animal.h"
-//Тест pull request`a
-
+#include "Header.h"
+#include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
+int main()
+{
+	Monkey		  Harambe;
+	Cow				Molly;
+	Worm			Anton;
+	Jellyfish		 Petr;
 
-int main() {
-	return 0;
+	Harambe.alive = true;
+	Harambe.milk_volume = 1;
+	Harambe.tail_lenght = 28;
+
+	Molly.alive = true;
+	Molly.milk_volume = 10;
+	Molly.white = true;
+
+	Anton.alive = true;
+	Anton.no_spine = 1;
+	Anton.length = 8;
+
+	Petr.alive = true;
+	Petr.no_spine = 1;
+	Petr.transparent = true;
+
+	cout << Harambe.about();
+	cout << Petr.about();
 }
