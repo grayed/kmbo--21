ab183a6df75b52eaa6fe2be263d3b9df963e2b5c
Egor Osipov <exilegrief@gmail.com>
2022-06-08 21:58:39 +0300

    .

--- memhacks/memhacks.cpp
+++ memhacks/memhacks.cpp
@@ -1,43 +1,24 @@
-﻿#include <iostream>
-#include "memhacks.h"
+﻿#include "memhacks.h"
+#include<ostream>
+#include<string>
+#include<sstream>
+#include<iostream>
 
 using namespace std;
 
-B::B() : b_s("It's b!") {
-	for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++)
-		data[i] = i * 2;
-}
 
 /// <summary>
 /// Выводит на экран адреса и размеры объекта типа <see cref="B"/> и его содержимого.
 /// Можно модифицировать для собственных отладочных целей.
 /// </summary>
 /// <param name="b">Изучаемый объект</param>
-void printInternals(const B& b) {
-	const A* a = &b, * a2 = a + 1;
+void printInternals(B& b) {
+	const A* a = &b, * a2 = a + 1; // указатель А* сомтрит на объект производного класса
 	cout << "Address of b is 0x" << &b << ", address of b.a_s is 0x" << &b.a_s << ", address of b.b_s is 0x" << &b.b_s << endl;
 	cout << "Size of A is " << sizeof(A) << ", size of B is " << sizeof(B) << endl;
 	cout << "B string is '" << b.getBString() << "'" << endl;
-	//cout << "B data: "; b.printData(cout); cout << endl;
-}
-
-/// <summary>
-/// Извлекает значение <see cref="B::b_s"/> из текущего объекта.
-/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
-/// </summary>
-/// <returns>Значение B::b_s</returns>
-std::string A::getBString() const {
-	// TODO
-}
-
-/// <summary>
-/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
-/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
-/// с помощью адресной арифметики.
-/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
-/// </summary>
-void A::printData(std::ostream& os) {
-	// TODO
+	cout << "B data: "; b.printData(cout); cout << endl;
+	cout << "B data: "; b.printData2(cout); cout << endl;
 }
 
 /// <summary>
@@ -45,13 +26,22 @@ void A::printData(std::ostream& os) {
 /// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
 /// с помощью виртуальных функций, предусмотренных в классе <see cref="A"/>.
 /// </summary>
-void A::printData2(std::ostream& os) {
-	// TODO
+void B::printData2(ostream& os) {
+	os << str_A();   // virtual function
+	os << str_B();
 }
+
+
 
 int main()
 {
+	A a;
 	B b;
+	cout << &a << "\n" << &b << "\n";
 	printInternals(b);
+	cout << "MEMHACKS:" << endl;
+	b.printData(cout);
+	cout << endl << "STANDART:" << endl;
+	b.printData2(cout);
 	return 0;
 }
--- memhacks/newhacks.cpp
+++ memhacks/newhacks.cpp
@@ -1,9 +1,78 @@
 ﻿#include <iostream>
+#include "newhacks.h"
 #include "memhacks.h"
 
 using namespace std;
 
+// 1 пункт
+void* Foo::operator new(size_t size /* размер в байтах */)
+{
+    auto p = malloc(size);      // ::operator new(size);
+    cerr << "operator new[] for Foo(" << size << ") returns " << p << endl;
+    return p;
+}
+
+void Foo::operator delete(void* p)
+{
+    cerr << "operator delete for Foo(" << p << ")" << endl;
+    free(p);    // ::operator delete(p);
+}
+
+// 2 пункт
+void* Bar::operator new(size_t size /* размер в байтах */)
+{
+    auto p = malloc(size);      // ::operator new(size);
+    cerr << "operator new for Bar(" << size << ") returns " << p << endl;
+    return p;
+}
+
+void Bar::operator delete(void* p)
+{
+    cerr << "operator delete for Bar(" << p << ")" << endl;
+    free(p);    // ::operator delete(p);
+}
+
+// 3 пункт
+Foo::Foo() { cerr << "constructed object Foo" << " at " << this << endl; }
+Foo::~Foo() { cerr << "deconstructed object Foo" << " at " << this << endl; }
+
+Bar::Bar() { cerr << "constructed object Bar" << " at " << this << endl; }
+Bar::~Bar() { cerr << "deconstructed object Bar" << " at " << this << endl; }
+
+Buz::Buz() { cerr << "constructed object Buz" << " at " << this << endl; }
+Buz::~Buz() { cerr << "deconstructed object Buz" << " at " << this << endl; }
+
+// 4 пункт
+void dynamicMemory() {
+    Foo* obj1 = new Foo, * obj2 = new Foo;
+    delete obj1;
+    delete obj2;
+
+    Bar* obj3 = new Bar;
+    delete obj3;
+}
+
+void stackMemory() {
+    Foo obj1, obj2;
+    Bar obj3;
+}
+
+// 5 пункт
+void* Buz::operator new(size_t size /* размер в байтах */)
+{
+    throw runtime_error("It's a forbidden action");
+}
+
+
 int main()
 {
-	return 0;
+    cout << "Explore dynamic memory" << endl;
+    dynamicMemory();
+
+    cout << "Explore stack memory" << endl;
+    stackMemory();
+
+    Buz* b = new Buz;
+
+    return 0;
 }
