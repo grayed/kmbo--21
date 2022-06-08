9c634d679d53323cc0eccebe036d53be83e28d38
Egor Osipov <exilegrief@gmail.com>
2022-06-08 21:58:39 +0300

    .

--- electricity/electricity.cpp
+++ electricity/electricity.cpp
@@ -6,19 +6,60 @@ using namespace std;
 bool Object::isConnectedTo(const Object& other) const
 {
     // TODO
+    for (int i = 1; i <= getPoleCount(); i++) {
+        for (int j = 1; j <= other.getPoleCount(); j++) {
+            if (getPole(i)->connectedObjectPole == other.getPole(j)->name && getPole(i)->connectedObject == const_cast<Object*>(&other))
+                return true;
+        }
+    }
+    return false;
+}
+
+bool Object::disconnect(const std::string& poleName)
+{
+    //TODO
+    if (getPole(poleName)->connectedObject != nullptr) {
+        getPole(getPole(poleName)->connectedObjectPole)->connectedObject = nullptr;
+        getPole(getPole(poleName)->connectedObjectPole)->connectedObjectPole = "";
+        getPole(poleName)->connectedObject = nullptr;
+        getPole(poleName)->connectedObjectPole = "";
+        return true;
+    }
     return false;
 }
 
 bool Object::connect(const std::string& poleName, const Object& other, const std::string& otherPoleName)
 {
-    // TODO
-    return false;
+    disconnect(poleName);
+    disconnect(otherPoleName);
+    getPole(poleName)->connectedObject = const_cast<Object*>(&other);
+    getPole(poleName)->connectedObjectPole = otherPoleName;
+    Pole* p = const_cast<Pole*>(other.getPole(otherPoleName));
+    p->connectedObjectPole = poleName;
+    p->connectedObject = const_cast<Object*>(this);
+    return true;
 }
+
 
 Switch::Switch(const std::string& name)
     : Object(name)
     , a1("A1")
     , a2("A2")
+{
+}
+
+Light::Light(const std::string& name)
+    : Object(name)
+    , a1("A1")
+    , a2("A2")
+{
+}
+
+Generator::Generator(const std::string& name)
+    : Object(name)
+    , a1("A1") //фаза - faza
+    , a2("A2") //нейтраль - neytral
+    , a3("A3") //Земля - Zemlai
 {
 }
 
@@ -34,6 +75,54 @@ const Pole* Switch::getPole(const string& name) const
 const Pole* Switch::getPole(size_t idx) const
 {
     // TODO
+    if (idx == 1)
+        return &a1;
+    if (idx == 2)
+        return &a2;
+    return nullptr;
+}
+
+
+
+const Pole* Light::getPole(size_t idx) const
+{
+
+    if (idx == 1)
+        return &a1;
+    if (idx == 2)
+        return &a2;
+    return nullptr;
+}
+
+const Pole* Light::getPole(const string& name) const
+{
+    if (name == a1.name)
+        return &a1;
+    if (name == a2.name)
+        return &a2;
+    return nullptr;
+}
+
+const Pole* Generator::getPole(size_t idx) const
+{
+
+    if (idx == 1)
+        return &a1;
+    if (idx == 2)
+        return &a2;
+    if (idx == 3)
+        return &a3;
+    return nullptr;
+}
+
+const Pole* Generator::getPole(const string& name) const
+{
+    if (name == a1.name)
+        return &a1;
+    if (name == a2.name)
+        return &a2;
+    if (name == a3.name)
+        return &a3;
     return nullptr;
 }
 
@@ -45,5 +134,17 @@ int main()
 
     // TODO: создать цепь из генератора, выключателя и светильника
 
+    Generator g; //A1 - фаза, А2 - нейтрал, А3 - земля
+    Light l;
+    Switch s;
+    // к А1(фазу) подключим переключатель к полюсу А2
+    g.connect("A1", s, "A2");
+    cout << "is " << (g.isConnectedTo(s) ? "" : "not ") << "connected" << endl;
+    //к другому полюсу переключателя подключим А1 подключаем лампу полюсу А2
+    s.connect("A1", l, "A2");
+    cout << "is " << (s.isConnectedTo(l) ? "" : "not ") << "connected" << endl;
+    // подключим лампу другим полюсам к генератору, нейтрал
+    g.connect("A2", l, "A1");
+    cout << "is " << (s.isConnectedTo(l) ? "" : "not ") << "connected" << endl;
     return 0;
 }
--- electricity/electricity.h
+++ electricity/electricity.h
@@ -50,7 +50,7 @@ protected:
     /// </summary>
     /// <param name="idx">Индекс полюса, от <c>0</c> до значения, возвращаемого <see cref="getPoleCount()"/>.</param>
     /// <returns>Полюс с указанным индексом, или <c>nullptr</c>, если такой полюс не существует.</returns>
-    Pole* getPole(size_t idx) { /* TODO */ return nullptr; }
+    Pole* getPole(size_t idx);
 
     /// <summary>
     /// Возвращает полюс по внутреннему индексу устройства.
@@ -63,8 +63,7 @@ public:
     virtual ~Object() {}
 
     const std::string& getName() const { return name; }
-    void getName(const std::string &newName) { name = newName; }
-
+    void getName(const std::string& newName) { name = newName; }
     /// <summary>
     /// Возвращает полюс по имени.
     /// </summary>
@@ -102,6 +101,15 @@ public:
     /// для которого вызывается этот метод.
     /// </remarks>
     bool connect(const std::string& poleName, const Object& other, const std::string& otherPoleName);
+    /// <резюме>
+    /// Отключает указанный полюс, если к нему что-либо подключено.
+    /// </summary>
+    /// <param name="poleName">Название полюса, от которого производится отключение.</param>
+    /// <returns><c>true</c> если что-либо было отключено, <c>false</c> в противном случае.</returns>
+    /// <примечания>
+    /// Вызов этого метода для полюса, если к нему ничего не подключено, не является ошибкой.
+    /// </примечания>
+    bool disconnect(const std::string& poleName);
 };
 
 /// <summary>
@@ -123,4 +131,32 @@ protected:
 
 // TODO: класс светильника с двумя полюсами
 
+class Light : public Object {
+public:
+    Pole a1, a2;
+
+    Light(const std::string& name = "");
+
+    virtual size_t getPoleCount() const { return 2; }
+
+    virtual const Pole* getPole(const std::string& name) const;
+
+protected:
+    virtual const Pole* getPole(size_t idx) const;
+};
+
 // TODO: класс генератора с тремя полюсами (фаза, нейтраль, земпя).
+
+class Generator : public Object {
+public:
+    Pole a1, a2, a3;
+
+    Generator(const std::string& name = "");
+
+    virtual size_t getPoleCount() const { return 3; }
+
+    virtual const Pole* getPole(const std::string& name) const;
+
+protected:
+    virtual const Pole* getPole(size_t idx) const;
+};
