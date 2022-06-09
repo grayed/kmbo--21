be97b4595c03829998c753bbeaa94d8f0be37f90
chernikovtimofey <chernikovtimofeyg@gmail.com>
2022-06-09 22:14:38 +0300

    memhacks solution

--- memhacks/memhacks.cpp
+++ memhacks/memhacks.cpp
@@ -1,11 +1,15 @@
 ﻿#include <iostream>
+#include <sstream>
 #include "memhacks.h"
 
 using namespace std;
 
+A::A() : a_s("It's a!") {}
+
 B::B() : b_s("It's b!") {
-	for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+	for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
 		data[i] = i * 2;
+	}
 }
 
 /// <summary>
@@ -17,18 +21,20 @@ void printInternals(const B& b) {
 	const A* a = &b, * a2 = a + 1;
 	cout << "Address of b is 0x" << &b << ", address of b.a_s is 0x" << &b.a_s << ", address of b.b_s is 0x" << &b.b_s << endl;
 	cout << "Size of A is " << sizeof(A) << ", size of B is " << sizeof(B) << endl;
+	cout << "A string is '" << b.getAString() << "'" << endl;
 	cout << "B string is '" << b.getBString() << "'" << endl;
-	//cout << "B data: "; b.printData(cout); cout << endl;
+}
+
+string A::getAString() const { 
+	return *((const string*) ((const long*) (this) + 1));
 }
 
 /// <summary>
 /// Извлекает значение <see cref="B::b_s"/> из текущего объекта.
 /// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
-/// </summary>
+/// </summary>S
 /// <returns>Значение B::b_s</returns>
-std::string A::getBString() const {
-	// TODO
-}
+std::string A::getBString() const { return *((const string*) (this + 1)); }
 
 /// <summary>
 /// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
@@ -36,8 +42,31 @@ std::string A::getBString() const {
 /// с помощью адресной арифметики.
 /// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
 /// </summary>
+
+float A::getdataFloat(int i) const { return ((float*)(this + 2) - 4)[i]; }
+
 void A::printData(std::ostream& os) {
-	// TODO
+	os << getAString() + " " + getBString() + " {";
+	for (int i = 0; i < 7; ++i) {
+		os << getdataFloat(i);
+		if (i != 6) os << ", ";
+	}
+	os << "}" << endl;
+}
+
+string A::getA_s() const { return a_s; }
+string B::getB_s() const { return b_s; }
+string B::getDataStr() const {
+	stringstream ss;
+	ss << "{ ";
+
+	for (int i = 0; i < 7; ++i) {
+		ss << data[i];
+		if (i != 6) ss << "," << " ";
+	}
+	ss << '}';
+
+	return ss.str();
 }
 
 /// <summary>
@@ -46,12 +75,14 @@ void A::printData(std::ostream& os) {
 /// с помощью виртуальных функций, предусмотренных в классе <see cref="A"/>.
 /// </summary>
 void A::printData2(std::ostream& os) {
-	// TODO
+	os << getA_s() << " " << getB_s() << " " << getDataStr() << endl;
 }
 
 int main()
 {
 	B b;
 	printInternals(b);
+	b.printData(cout);
+	b.printData2(cout);
 	return 0;
 }
--- memhacks/memhacks.h
+++ memhacks/memhacks.h
@@ -10,11 +10,19 @@ class A {
 	int foo;
 
 	friend void printInternals(const B&);
+	virtual std::string getB_s() const { return " "; };
+	virtual std::string getDataStr() const { return " "; };
 
 public:
+	std::string getAString() const;
 	std::string getBString() const;
+	float getdataFloat(int i) const;
 	void printData(std::ostream& os);
 	void printData2(std::ostream& os);
+
+	std::string getA_s() const;
+
+	A();
 };
 
 class B : public A {
@@ -24,6 +32,9 @@ class B : public A {
 	friend void printInternals(const B&);
 
 public:
+	std::string getB_s() const override;
+	std::string getDataStr() const override;
+
 	B();
 };
 
