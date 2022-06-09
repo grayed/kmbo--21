6a5f1fde1fe4d41f359c3b6cf25e228bc0b8cca6
DaniilBakulin <90862498+DaniilBakulin@users.noreply.github.com>
2022-06-09 16:00:33 +0300

    Update vector.h

--- vectors/vector.h
+++ vectors/vector.h
@@ -1,19 +1,27 @@
 #pragma once
-
-#include <ostream>
-
-class vector3d {
+#include<ostream>
+using namespace std;
+class vector3d
+{
 	float data[3];
 
-public:
+public :
 	vector3d() { data[2] = data[1] = data[0] = 0; }
 	vector3d(float value) { data[2] = data[1] = data[0] = value; }
-	vector3d(float a1, float a2, float a3) { data[0] = a1;  data[1] = a2; data[2] = a3; }
+	vector3d(float a1, float a2, float a3) { data[2] = a3, data[1] = a2, data[0] = a1; }
 
-	float& operator[](int idx)       { return data[idx]; }
-	float  operator[](int idx) const { return data[idx]; }
-
+	float& operator[](int idx) { return data[idx]; }
+	float operator[](int idx) const { return data[idx]; }
 	friend int main(int argc, char** argv);
 };
+ostream& operator<<(ostream& os, const vector3d& v);
+vector3d operator -(const vector3d& v);
+vector3d operator -(const vector3d& v1, const vector3d& v2);
+vector3d operator +(const vector3d& v1, const vector3d& v2);
+vector3d operator *(const vector3d& v, float n);
+vector3d operator *(float n, const vector3d& v);
+vector3d operator /(const vector3d& v, float n);
+vector3d operator /(float n, const vector3d& v);
+vector3d operator !(const vector3d& v);
 
-std::ostream& operator <<(std::ostream& os, const vector3d& v);
+bool test_vector3d();
