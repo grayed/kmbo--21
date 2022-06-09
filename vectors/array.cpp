30577c7b627f6c3dc521ccd5ac01cde4dcdf6792
DaniilBakulin <90862498+DaniilBakulin@users.noreply.github.com>
2022-06-09 16:23:12 +0300

    Update array.cpp

--- vectors/array.cpp
+++ vectors/array.cpp
@@ -1,45 +1,23 @@
-#include <iostream>
-using namespace std;
+#include "array.h"
 
-class Vector3d {
-    float data[3];
-public:
-    Vector3d() { data[2] = data[1] = data[0] = 0; }
-    Vector3d(float value) { data[2] = data[1] = data[0] = value; }
-    float  operator[] (int index) const { cerr << "const" << index << endl; return data[index]; }
-    float& operator[] (int index)       { cerr << "non-const" << index << endl; return data[index]; }
-};
-
-template<size_t Dimensions>
-class Vector {
-    float data[Dimensions];
-public:
-    Vector(float value = 0) { for (size_t i = 0; i < Dimensions; i++) data[i] = value;  }
-    float  operator[] (size_t index) const { return data[index]; }
-    float& operator[] (size_t index)       { return data[index]; }
-};
-
-// Написать шаблонные функции:
-//   1. operator + (реализация внутри класса)
-//   2. operator * (вектор на скаляр и скаляр на вектор; реализация вне класса)
-//   3. Нахождение среднего для двух векторов (реализация вне класса)
-
-ostream& operator <<(ostream& os, const Vector3d& v) {
-    return os << "{ " << v[0] << ", " << v[1] << ", " << v[2] << " }";
-}
-
-template<size_t Dimensions>
-ostream& operator <<(ostream& os, const Vector<Dimensions>& v) {
-    os << "{ " << v[0];
-    for (size_t i = 1; i < Dimensions; i++)
-        os << ", " << v[i];
-    return os << " }";
+template<size_t dim>
+vector<dim> average(const vector<dim> a, const vector<dim> b) {
+    vector<dim> temp;
+    for (int i = 0; i < dim; ++i) temp[i] = (a[i] + b[i]) / 2;
+    return temp;
 }
 
 int main() {
-    Vector<12> v1;
-    v1[1] = v1[2] = 54;
-    Vector<12> v2 = v1;
-    cout << v2 << endl;
-	return 0;
+    vector<5> vec_1;
+
+    for (int i = 0; i < vec_1.size(); ++i) vec_1[i] = (float)i;
+
+    vector<5> vec_2 = vec_1 * 3;
+    vector<5> vec_3 = vec_1 * 4;
+
+    std::cout << "vec_1 * 3: " << vec_2 << std::endl;
+    std::cout << "vec_1 * 4: " << vec_3 << std::endl;
+    std::cout << "average of vec_2 and vec_3: " << average(vec_2, vec_3) << std::endl;
+
+    return 0;
 }
