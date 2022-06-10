161dd11bc32fe4b8cb880e66ed9f3fb411118717
chernikovtimofey <chernikovtimofeyg@gmail.com>
2022-06-10 15:59:35 +0300

    newhack solution

--- memhacks/newhacks.h
+++ memhacks/newhacks.h
@@ -1,2 +1,32 @@
-﻿#pragma once
+﻿#include <iostream>
+#pragma once
 
+class Foo {
+    std::string str;
+    int i;
+
+public:
+    Foo();
+    ~Foo();
+
+    void* operator new(size_t size);
+    void operator delete(void* foo);
+
+};
+
+class Bar : public Foo {
+public:
+    Bar();
+    ~Bar();
+
+    void* operator new(size_t size);
+    void operator delete(void* bar);
+};
+
+class Buz : public Foo {
+    void* operator new(size_t size);
+
+public:
+    Buz();
+    ~Buz(); 
+};
