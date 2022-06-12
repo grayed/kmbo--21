25ff0a87633163a1417b0a5e078aff26be4d092b
Елизавета Сидорова <2003sev@mail.ru>
2022-06-12 19:32:28 +0300

    Animals 2

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -2,78 +2,18 @@
 #include <string>
 using namespace std;
 
-string Animal::about() const {
-	stringstream ss;
-	ss << "Weight = " << weight << '\n' << "Height = " << height;
-	return ss.str();
-}
-
-string Bird::about() const {
-	stringstream ss;
-	ss << Animal::about() << '\n' << "Ability to fly = " << fly << '\n' << "Wingspan =" << wingspan;
-	return ss.str();
-}
-
-string Penguin::about() const {
-	stringstream ss;
-	ss << Bird::about() << '\n' << "Sex = " << sex;
-	return ss.str();
-}
-
-string Parrot::about() const {
-	stringstream ss;
-	ss << Bird::about() << '\n' << "Ability to speak = " << speak;
-	return ss.str();
-}
-
-string Mammal::about() const {
-	stringstream ss;
-	ss << Animal::about() << '\n' << "Duration of pregnancy = " << pregnancyDuration << '\n' << "Age =" << age;
-	return ss.str();
-}
-
-string Dog::about() const {
-	stringstream ss;
-	ss << Mammal::about() << '\n' << "Color of wool = " << woolcolor;
-	return ss.str();
-}
-
-string Human::about() const {
-	stringstream ss;
-	ss << Mammal::about() << '\n' << "IQ = " << iq;
-	return ss.str();
-}
-
 int main() {
-	Penguin Skipper;
-	Skipper.weight = 3;
-	Skipper.height = 0.4;
-	Skipper.fly = false;
-	Skipper.wingspan = 40;
-	Skipper.sex = "Male";
-	cout << "Skipper : " << Skipper.about() << endl << endl;
+	Penguin Skipper(3, 0.4, false, 40, "Male");
+	cout << "Skipper: " << Skipper.about() << endl << endl;
 
-	Parrot Kesha;
-	Kesha.weight = 0.04;
-	Kesha.height = 0.18;
-	Kesha.fly = true;
-	Kesha.wingspan = 30;
-	Kesha.speak = true;
-	cout << "Kesha : " << Kesha.about() << endl << endl;
+	Parrot Kesha(0.04, 0.18, true, 30, true);
+	cout << "Kesha: " << Kesha.about() << endl << endl;
 
-	Dog Masyanya;
-	Masyanya.weight = 30;
-	Masyanya.height = 0.57;
-	Masyanya.pregnancyDuration = 63;
-	Masyanya.age = 10;
-	Masyanya.woolcolor = "White";
-	cout << "Masyanya : " << Masyanya.about() << endl << endl;
+	Dog Masyanya(30, 0.57, 63, 10, "White");
+	cout << "Masyanya: " << Masyanya.about() << endl << endl;
 
-	Human Vasilisa;
-	Vasilisa.weight = 50;
-	Vasilisa.height = 1.70;
-	Vasilisa.pregnancyDuration = 280;
-	Vasilisa.age = 30;
-	Vasilisa.iq = 126;
-	cout << "Vasilisa : " << Vasilisa.about() << endl;
+	Human Vasilisa(50, 1.70, 280, 30, 126);
+	cout << "Vasilisa: " << Vasilisa.about() << endl << endl;
+
+	return 0;
 }
--- animals/animal.h
+++ animals/animal.h
@@ -5,61 +5,106 @@
 using namespace std;
 
 class Animal {
-public:
+private:
 	float weight;	// kg
 	float height; // meters
+protected:
+	Animal() {};
+	Animal(float weight_, float height_) { weight = weight_; height = height_; }
+public:
+	float getWeight() const { return weight; }
+	void setWeight(float newWeight) { weight = newWeight; }
+	float getHeight() const { return height; }
+	void setHeight(float newHeight) { height = newHeight; }
 
-	virtual std::string about() const;
+	virtual string about() const { return (stringstream() << "Weight = " << weight << '\n' << "Height = " << height).str(); }
 };
 
 class Mammal : public Animal {
-public:
+private:
 	float pregnancyDuration;	// days
 	float age;                  //years
+protected:
+	Mammal() {};
+	Mammal(float weight_, float height_, float pregnancyDuration_, float age_) :Animal(weight_, height_) { pregnancyDuration = pregnancyDuration_; age = age_; };
+public:
+	float getPregnancyDuration() const { return pregnancyDuration; }
+	void setPregnancyDuration(float newPregnancyDuration) { pregnancyDuration = newPregnancyDuration; }
+	float getAge() const { return age; }
+	void setAge(float newAge) { age = newAge; }
 
-	virtual std::string about() const;
+	virtual string about() const { return (stringstream() << Animal::about() << '\n' << "Duration of pregnancy = " << pregnancyDuration << '\n' << "Age = " << age).str(); }
 };
 
 class Dog : public Mammal {
-public:
+private:
 	string woolcolor; //color
+public:
+	Dog() {};
+	Dog(float weight_, float height_, float pregnancyDuration_, float age_, string woolcolor_) :Mammal(weight_, height_, pregnancyDuration_, age_) {
+		woolcolor = woolcolor_;
+	}
 
-	virtual std::string about() const;
+	string getWoolcolor() const { return woolcolor; }
+	void setWoolcolor(string newWoolcolor) { woolcolor = newWoolcolor; }
+
+	virtual string about() const { return (stringstream() << Mammal::about() << '\n' << "Woolcolor = " << woolcolor).str(); }
 };
 
 class Human : public Mammal {
-public:
+private:
 	int iq;      //scores
+public:
+	Human() {};
+	Human(float weight_, float height_, float pregnancyDuration_, float age_, int iq_) :Mammal(weight_, height_, pregnancyDuration_, age_) {
+		iq = iq_;
+	}
+	int getIQ() const { return iq; }
+	void setIQ(int newIQ) { iq = newIQ; }
 
-	virtual std::string about() const;
+	virtual string about() const { return (stringstream() << Mammal::about() << '\n' << "IQ = " << iq).str(); }
 };
 
 class Bird : public Animal {
-public:
+private:
 	bool fly;	      // true/false 
 	float wingspan;   //centimeters
+protected:
+	Bird() {};
+	Bird(float weight_, float height_, bool fly_, float wingspan_) :Animal(weight_, height_) { fly = fly_; wingspan = wingspan_; };
+public:
+	bool getFly() const { return fly; }
+	void setFly(bool newFly) { fly = newFly; }
+	float getWingspan() const { return wingspan; }
+	void setWingspan(float newWingspan) { wingspan = newWingspan; }
 
-	virtual std::string about() const;
+	virtual string about() const { return (stringstream() << Animal::about() << '\n' << "Fly = " << fly << '\n' << "Wingspan = " << wingspan).str(); }
 };
 
 class Penguin : public Bird {
-public:
+private:
 	string sex;       //male/female
+public:
+	Penguin() {};
+	Penguin(float weight_, float height_, bool fly_, float wingspan_, string sex_) :Bird(weight_, height_, fly_, wingspan_) {
+		sex = sex_;
+	}
+	string getSex() const { return sex; }
+	void setSex(string newSex) { sex = newSex; }
 
-	virtual std::string about() const;
+	virtual string about() const { return (stringstream() << Bird::about() << '\n' << "Sex = " << sex).str(); }
 };
 
 class Parrot : public Bird {
-public:
+private:
 	bool speak;    //true/false
+public:
+	Parrot() {};
+	Parrot(float weight_, float height_, bool fly_, float wingspan_, bool speak_) :Bird(weight_, height_, fly_, wingspan_) {
+		speak = speak_;
+	}
+	bool getSpeak() const { return speak; }
+	void setSpeak(bool newSpeak) { speak = newSpeak; }
 
-	virtual std::string about() const;
+	virtual string about() const { return (stringstream() << Bird::about() << '\n' << "Speak " << speak).str(); }
 };
-
-inline std::ostream& operator <<(std::ostream& os, const Penguin& penguin) {
-	return os << penguin.about();
-}
-
-inline std::ostream& operator <<(std::ostream& os, const Parrot& parrot) {
-	return os << parrot.about();
-}
