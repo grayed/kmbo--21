826d651a7374933e4bf83fdf3c4b73b7588a4ae1
fert1414 <retro.fert11@mail.ru>
2022-06-14 16:29:28 +0300

    memhacks #2

--- memhacks/newhacks.h
+++ memhacks/newhacks.h
@@ -1,2 +1,34 @@
 ﻿#pragma once
 
+#include <iostream>
+
+using namespace std;
+
+class Foo {
+    string str;
+    int value;
+
+public:
+    Foo();
+    ~Foo();
+
+    void* operator new(size_t size);
+    void operator delete(void* foo);
+};
+
+class Bar : public Foo {
+public: 
+    Bar();
+    ~Bar();
+
+    void* operator new(size_t size);
+    void operator delete(void* bar);
+};
+
+class Buz : public Foo {
+    void* operator new(size_t size);
+
+public:
+    Buz();
+    ~Buz();
+};
