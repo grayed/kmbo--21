404c4b4e6299cdc957ad00ed32a78932904523ce
vanek1337228 <90497093+vanek1337228@users.noreply.github.com>
2022-06-15 12:42:03 +0300

    memhacks, newhacks

---
+++ memhacks/newhacks.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <string>
+
+
+class foo
+{
+   int x;
+   std:: string str="Hello world";
+public:
+   static void* operator new (size_t size);
+   static void operator delete (void *p);
+};
+
+class Bar : foo
+{
+   char data[20];
+   bool t;
+public:
+   static void* operator new (size_t size);
+   static void operator delete (void *p);
+};
+class Buz : foo
+{
+   double y;
+   float b;
+public:
+   static void operator delete (void *p);
+private:
+   static void* operator new (size_t size);
+};
