3ac1deafde3e64e7aa8663d860a13b7a45f86fc8
RomanPozdn <90093720+RomanPozdn@users.noreply.github.com>
2022-06-15 14:11:19 +0300

    Мемхакс 1 сделан

--- memhacks/memhacks.h
+++ memhacks/memhacks.h
@@ -1,30 +1,53 @@
-﻿#pragma once
-
+#pragma once
+using namespace std;
 #include <ostream>
 #include <string>
+#include <sstream>
+#include <iostream>
 
 class B;	// чтобы можно было объявить printInternals() как friend в классе A
 
 class A {
-	std::string a_s;
+	string a_s;
 	int foo;
-
 	friend void printInternals(const B&);
-
+	friend void printInternals(B&);
 public:
-	std::string getBString() const;
-	void printData(std::ostream& os);
-	void printData2(std::ostream& os);
+	A() : a_s("It's a"), foo(0) { }
+
+	string getAString() const { 
+		return *((const string*)((const float*)(this + 1) - 12));}
+	string getBString() const {
+		return  *((const string*)(this + 1));}
+
+	float getdataFloat(int i) { return ((float*)(this + 2) - 4)[i]; }
+
+	virtual string str_A() const {
+		stringstream ss;
+		ss << "String A: " << a_s;
+		return ss.str();}
 };
 
 class B : public A {
-	std::string b_s;
+	string b_s;
 	float data[7];
-
 	friend void printInternals(const B&);
-
+	friend void printInternals(B&);
 public:
-	B();
-};
+	B() : b_s("It's b") {
+		for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+			data[i] = (float)i * 2;}
 
-void printInternals(const B& b);
+	virtual string str_B() {
+		stringstream ss;
+		ss << "String B: " << b_s << std::endl;
+		ss << "Data : ";
+		for (int i = 0; i < 7; i++) {ss << data[i] << "; ";}
+		std::cout << std::endl;
+		return ss.str();}
+	void printData2(std::ostream& os);
+	void printData(std::ostream& os) {
+		os << "A string = '" << getAString() << "', B string = '" << getBString() << "'" << std::endl;
+		for (int i = 0; i < 7; ++i) os << getdataFloat(i) << " ";}
+
+};
