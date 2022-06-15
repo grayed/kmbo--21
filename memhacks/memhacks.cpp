cf547408aec3b6ea0053b7240fc716f1a7aa6eb7
RomanPozdn <90093720+RomanPozdn@users.noreply.github.com>
2022-06-15 14:11:44 +0300

    Update memhacks.cpp

--- memhacks/memhacks.cpp
+++ memhacks/memhacks.cpp
@@ -3,55 +3,26 @@
 
 using namespace std;
 
-B::B() : b_s("It's b!") {
-	for (auto i = 0; i < sizeof(data) / sizeof(data[0]); i++)
-		data[i] = i * 2;
+void printInternals(B& b) {
+	const A* a = &b, * a2 = a + 1; 
+	std::cout << "Address of b is 0x" << &b << ", address of b.a_s is 0x" << &b.a_s << ", address of b.b_s is 0x" << &b.b_s << std::endl;
+	std::cout << "Size of A is " << sizeof(A) << ", size of B is " << sizeof(B) << std::endl;
+	std::cout << "B string is '" << b.getBString() << "'" << std::endl;
+	std::cout << "B data: "; b.printData(std::cout); std::cout << std::endl;
+	std::cout << "B data: "; b.printData2(std::cout); std::cout << std::endl;
 }
 
-/// <summary>
-/// Выводит на экран адреса и размеры объекта типа <see cref="B"/> и его содержимого.
-/// Можно модифицировать для собственных отладочных целей.
-/// </summary>
-/// <param name="b">Изучаемый объект</param>
-void printInternals(const B& b) {
-	const A* a = &b, * a2 = a + 1;
-	cout << "Address of b is 0x" << &b << ", address of b.a_s is 0x" << &b.a_s << ", address of b.b_s is 0x" << &b.b_s << endl;
-	cout << "Size of A is " << sizeof(A) << ", size of B is " << sizeof(B) << endl;
-	cout << "B string is '" << b.getBString() << "'" << endl;
-	//cout << "B data: "; b.printData(cout); cout << endl;
-}
+void B::printData2(std::ostream& os) {
+	os << str_A();
+	os << str_B();}
 
-/// <summary>
-/// Извлекает значение <see cref="B::b_s"/> из текущего объекта.
-/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
-/// </summary>
-/// <returns>Значение B::b_s</returns>
-std::string A::getBString() const {
-	// TODO
-}
-
-/// <summary>
-/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
-/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
-/// с помощью адресной арифметики.
-/// Подразумевается, что текущий объект на самом деле представлено классом <see cref="B"/>.
-/// </summary>
-void A::printData(std::ostream& os) {
-	// TODO
-}
-
-/// <summary>
-/// Извлекает значения <see cref="A::a_s"/>, <see cref="B::b_s"/> и <see cref="B::data"/>
-/// из текущего объекта и выводит их в текстовом виде в указанный выходной поток
-/// с помощью виртуальных функций, предусмотренных в классе <see cref="A"/>.
-/// </summary>
-void A::printData2(std::ostream& os) {
-	// TODO
-}
-
-int main()
-{
+int main(){
+	A a;
 	B b;
+	cout << &a << "\n" << &b << "\n";
 	printInternals(b);
-	return 0;
-}
+	cout << "MEMHACKS:" << endl;
+	b.printData(cout);
+	cout << endl << "STANDART:" << endl;
+	b.printData2(cout);
+	return 0;}
