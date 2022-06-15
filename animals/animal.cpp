5785ac99c5199b8d737659457c72c2aecb1b4333
Sashkachick <muravevaaleksandra9@gmail.com>
2022-06-15 13:31:48 +0300

    animals

--- animals/animal.cpp
+++ animals/animal.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
+	Cat kity(10.5, 9, 5.5);
+	kity.setWeight(9);
+	int weigth = kity.getWeight();
+	Shark sharky(550, 40, 350);
+	sharky.setJawSize(200);
+	sharky.setSize(700);
+	Giraffe giraffy (300.56, 15, 1500);
+	giraffy.setpregnancyDuration(10);
+	cout << sharky.getJawSize() << "\t" << sharky.getSize();
 	return 0;
-}
+};
+
+
